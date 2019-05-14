f3fffc0b41c0e666a1e40988cf73b88c1e845e0e
jlashner <jlashner@gmail.com>
2019-05-13 21:56:18 -0700

    moved agents from OCS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# socs
-A Repository for Simons Observatory specific OCS agents and clients
+# SOCS:  The Simons Observatory Control System
+
+This is a repository where we can store SO-specific agents and clients, to keep the OCS library general.
+Make sure to add the `agents` directory to you ocs site-config file.
---
+++ agents/ocs_plugin_so.py
@@ -0,0 +1,16 @@
+"""
+Register our agents in ocs central.  In order for this script to
+be imported by site_config.scan_for_agents(), it must be in the python
+path and called something like ocs_plugin_*.
+"""
+
+import ocs
+import os
+root = os.path.abspath(os.path.split(__file__)[0])
+
+for n,f in [
+        ('Lakeshore372Agent', 'thermometry/LS372_agent.py'),
+        ('Lakeshore240Agent', 'thermometry/LS240_agent.py'),
+        ('Keithley2230G-PSU', 'keithley2230G-psu/keithley_agent.py'),
+]:
+    ocs.site_config.register_agent_class(n, os.path.join(root, f))
