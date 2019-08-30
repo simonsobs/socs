9cd02ec0b3d0b0c5271abbc5757b3459dc8b83c3
Brian Koopman <brian.koopman@yale.edu>
2019-08-30 18:42:59 -0400

    Add documentation and rename objects and functions

--- agents/pysmurf_aggregator/stream_listener.py
+++ agents/pysmurf_aggregator/stream_listener.py
@@ -43,17 +43,69 @@ def _create_file_path(start_time, data_dir):
     return filepath
 
 
-class G3StreamListener:
-    def __init__(self, agent, time_per_file, data_dir, address='localhost', port=4536):
+class G3StreamAggregator:
+    """Aggregator for G3 data streams sent over the G3NetworkSender.
+
+    This Agent is built to work with the SMuRF data streamer, which collects
+    data from a SMuRF blade, packages it into a G3 Frame and sends it over the
+    network via a G3NetworkSender.
+
+    We read from the specified address with a G3Reader, and write the data to
+    .g3 files on disk.
+
+    Parameters
+    ----------
+    agent : OCSAgent
+        OCSAgent object which forms this Agent
+    time_per_file : int
+        Amount of time in seconds to have in each file. Files will be rotated
+        after this many seconds
+    data_dir : str
+        Location to write data files to. Subdirectories will be created within
+        this directory roughly corresponding to one day.
+    address : str
+        Address of the host sending the data via the G3NetworkSender
+    port : int
+        Port to listen for data on
+
+    Attributes
+    ----------
+    agent : OCSAgent
+        OCSAgent object which forms this Agent
+    time_per_file : int
+        Amount of time in seconds to have in each file. Files will be rotated
+        after this many seconds.
+    data_dir : str
+        Location to write data files to. Subdirectories will be created within
+        this directory roughly corresponding to one day.
+    address : str
+        Address of the host sending the data via the G3NetworkSender.
+    port : int
+        Port to listen for data on.
+    is_streaming : bool
+        Tracks whether or not the aggregator is writing to disk. Setting to
+        false stops the aggregation of data.
+    log : txaio.tx.Logger
+        txaio logger object, created by the OCSAgent
+
+    """
+
+    def __init__(self, agent, time_per_file, data_dir, address='localhost',
+                 port=4536):
         self.agent = agent
-        self.log = self.agent.log
+        self.time_per_file = time_per_file
+        self.data_dir = data_dir
         self.address = address
         self.port = port
         self.is_streaming = False
-        self.time_per_file = time_per_file
-        self.data_dir = data_dir
+        self.log = self.agent.log
 
-    def start_stream(self, session, params=None):
+    def start_aggregation(self, session, params=None):
+        """start_aggregation(params=None)
+
+        OCS Process to start data aggregation.
+
+        """
         if params is None:
             params = {}
 
@@ -94,11 +146,16 @@ class G3StreamListener:
             writer(core.G3Frame(core.G3FrameType.EndProcessing))
             writer = None
 
-        return True, "Finished Streaming"
+        return True, "Finished streaming"
 
-    def stop_stream(self, session, params=None):
+    def stop_aggregation(self, session, params=None):
+        """stop_aggregation(params=None)
+
+        Stop method associated with start_aggregation process.
+
+        """
         self.is_streaming = False
-        return True, "Stopping Streaming"
+        return True, "Stopping streaming"
 
 
 if __name__ == '__main__':
@@ -113,18 +170,18 @@ if __name__ == '__main__':
     pgroup.add_argument('--address', default='localhost')
 
     args = parser.parse_args()
-    site_config.reparse_args(args, 'G3StreamListener')
+    site_config.reparse_args(args, 'G3StreamAggregator')
 
     agent, runner = ocs_agent.init_site_agent(args)
-    listener = G3StreamListener(agent,
-                                int(args.time_per_file),
-                                args.data_dir,
-                                address=args.address,
-                                port=int(args.port))
+    listener = G3StreamAggregator(agent,
+                                  int(args.time_per_file),
+                                  args.data_dir,
+                                  address=args.address,
+                                  port=int(args.port))
 
     agent.register_process('stream',
-                           listener.start_stream,
-                           listener.stop_stream,
+                           listener.start_aggregation,
+                           listener.stop_aggregation,
                            startup=bool(args.auto_start))
 
     runner.run(agent, auto_reconnect=True)
