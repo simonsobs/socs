84409d2f99f6f5ef7af045f2ecd5e2df277b644f
Lauren Saunders <lauren.saunders@yale.edu>
2019-09-07 23:51:50 +0000

    added docstring and label clarity, improved reading time

--- socs/smurf_g3_reader/smurf_g3_reader.py
+++ socs/smurf_g3_reader/smurf_g3_reader.py
@@ -5,30 +5,36 @@ import pickle
 import datetime
 import sys, os
 
-def g3_to_dict(g3file):
-    
+def g3_to_array(g3file):
+    '''
+    Takes a G3 file output from the SMuRF archiver and reads to a numpy array.
+
+    Arguments
+    ---------
+    g3file : full path to the G3 file
+    '''
     frames = [fr for fr in core.G3File(g3file)]
     
     data=[]
 
-    times = []
+    frametimes = []
     for frame in frames:
-        for time in frame['data'].times():
-            times.append(str(time))
-    times = np.asarray(times)
+        frametime = frame['data'].times()
+        frametimes.append(frametime)
+    times = np.hstack(frametimes)
     
     channums = []
     for chan in frames[0]['data'].keys():
         channums.append(int(chan))
     channums.sort()
     for ch in channums:
-        print('Adding channel %s'%ch)
+ #       print('Adding channel %s'%ch)
         chdata = []
         for frame in frames:
-            for i in range(len(frame['data'][str(ch)])):
-                chdata.append(frame['data'][str(ch)][i])
-        chdata = np.asarray(chdata)
-        data.append(chdata)
+            framedata = frame['data'][str(ch)]
+            chdata.append(framedata)
+        chdata_all = np.hstack(chdata)
+        data.append(chdata_all)
 
     data = np.asarray(data)
     return times, data
@@ -36,8 +42,8 @@ def g3_to_dict(g3file):
 
 if __name__ == '__main__':
     g3file = sys.argv[1]
-    times, data = g3_to_dict(g3file)
-    newfile_name = g3file.strip('.g3')
+    times, data = g3_to_array(g3file)
+    newfile_name = g3file.split('/')[-1].strip('.g3')
     with open(newfile_name+'.pkl','wb') as f:
         pickle.dump({'times':times, 'data':data},f)
     f.close()
