b57dfeab6c2932752a555da0f2c7372f940a7b52
jseibert575 <33437183+jseibert575@users.noreply.github.com>
2020-01-30 09:56:04 -0800

    Update agent.py file to handle bad compressor output strings (#33)
    * Update agent.py file to handle bad compressor output strings
    
    If the compressor spits out a bad string, the agent skips that data block and prints the output into logs
    
    * Made changes requested for pull request. Reformated to reduce columns, added radix of 16, and moved 'sleep' statement after 'publish'
    
    * Add testing folder: Not for general release
    
    * Implement error testing
    
    Co-authored-by: JakeSpisak <43190235+JakeSpisak@users.noreply.github.com>

--- agents/cryomech-cpa1114/cryomech_cpa1114_agent.py
+++ agents/cryomech-cpa1114/cryomech_cpa1114_agent.py
@@ -1,5 +1,5 @@
 #script to log and readout PTC data through ethernet connection
-#Tamar Ervin, September 2019
+#Tamar Ervin and Jake Spisak, December 2019
 
 import sys, os
 import binascii
@@ -11,6 +11,7 @@ import errno
 from contextlib import contextmanager
 from ocs import site_config, ocs_agent
 from ocs.ocs_twisted import TimeoutLock
+import random
 
 STX = '\x02'
 ADDR = '\x10'
@@ -37,12 +38,13 @@ def time_limit(seconds):
         signal.alarm(0)
 
 class PTC:
-    def __init__(self, ip_address, port=502, timeout=10):
+    def __init__(self, ip_address, port=502, timeout=10, fake_errors=False):
         self.ip_address = ip_address
         self.port = port  
+        self.fake_errors = fake_errors
         
         self.comm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.comm.connect((self.ip_address, self.port))
+        self.comm.connect((self.ip_address, self.port)) #connects to the PTC
         self.comm.settimeout(timeout)
                  
     def get_data(self):
@@ -51,51 +53,82 @@ class PTC:
         """
         self.comm.sendall(self.buildRegistersQuery()) 
         data = self.comm.recv(1024)
-        brd = self.breakdownReplyData(data)
+        data_flag, brd = self.breakdownReplyData(data)
             
-        return brd    
+        return data_flag, brd    
     
     def buildRegistersQuery(self):
         query = bytes([0x09, 0x99,  # Message ID
                        0x00, 0x00,  # Unused
                        0x00, 0x06,  # Message size in bytes
-                       0x01,        # Slave Address
-                       0x04,        # Function Code  3= Read HOLDING registers, 4 read INPUT registers
+                       0x01,        # Slave Address                       
+                       0x04,        # Function Code  
                        0x00,0x01,   # The starting Register Number
                        0x00,0x35])  # How many to read
         return query
 
     def breakdownReplyData(self, rawdata):
         """
-        Take in raw ptc data, and return a dictionary. The dictionary keys are the data labels, 
+        Take in raw ptc data, and return a dictionary. 
+        The dictionary keys are the data labels, 
         the dictionary values are the data in floats or ints. 
         """
         
         #Associations between keys and their location in rawData
-        keyloc = {"Operating State": [9,10], "Pump State": [11, 12], "Warnings": [15, 16, 13, 14], "Alarms":[19, 20, 17, 18],
-                  "Coolant In": [22, 21, 24, 23], "Coolant Out": [26, 25, 28, 27], "Oil": [30, 29, 32, 31], 
-                  "Helium": [34, 33, 36, 35], "Low Pressure": [38, 37, 40, 39], "Low Pressure Average":[42, 41, 44, 43],
-                  "High Pressure": [46,45,48,47], "High Pressure Average": [50, 49, 52, 51], "Delta Pressure": [54, 53, 56, 55],
+        keyloc = {"Operating State": [9,10], 
+                  "Pump State": [11, 12], 
+                  "Warnings": [15, 16, 13, 14], 
+                  "Alarms":[19, 20, 17, 18],
+                  "Coolant In": [22, 21, 24, 23], 
+                  "Coolant Out": [26, 25, 28, 27], 
+                  "Oil": [30, 29, 32, 31], 
+                  "Helium": [34, 33, 36, 35], 
+                  "Low Pressure": [38, 37, 40, 39], 
+                  "Low Pressure Average":[42, 41, 44, 43],
+                  "High Pressure": [46,45,48,47], 
+                  "High Pressure Average": [50, 49, 52, 51], 
+                  "Delta Pressure": [54, 53, 56, 55], 
                   "Motor Current": [58, 57, 60, 59]}
         
-        #Iterate through all keys and return the data in a usable format
+        #Iterate through all keys and return the data in a usable format. 
+        #If there is an error in the string format, print the  
+        # error to logs, return an empty dictionary, and flag the data as bad
         data = {}
-        for key in keyloc.keys():
-            locs = keyloc[key] 
-            wkrBytes = bytes([rawdata[loc] for loc in locs])
-            
-            #three different data formats to unpack
-            if key in ["Operating State", "Pump State"]:
-                state = int.from_bytes(wkrBytes, byteorder='big')
-                data[key] = state
-            
-            if key in ["Warnings", "Alarms"]:
-                data[key] = int(''.join('{:02x}'.format(x) for x in wkrBytes))
-                
-            if key in ["Coolant In", "Coolant Out", "Oil", "Helium", "Low Pressure", "Low Pressure Average","High Pressure", "High Pressure Average", "Delta Pressure","Motor Current"]:
-                data[key] = struct.unpack('f', wkrBytes)[0]
-                
-        return data
+        
+        #If fake_errors=True, then randomly output the string 'FAKE ERROR' 
+        #instead of the actual data 50% of the time
+        if self.fake_errors:
+            if random.random() < 0.5:
+                rawdata = "FAKE ERROR" 
+        
+        try:
+            for key in keyloc.keys():
+                locs = keyloc[key] 
+                wkrBytes = bytes([rawdata[loc] for loc in locs])
+
+                #three different data formats to unpack
+                if key in ["Operating State", "Pump State"]:
+                    state = int.from_bytes(wkrBytes, byteorder='big')
+                    data[key] = state
+
+                if key in ["Warnings", "Alarms"]:
+                    data[key] = int(''.join('{:02x}'.format(x) 
+                                            for x in wkrBytes), 16)
+
+                if key in ["Coolant In", "Coolant Out", "Oil", "Helium", 
+                           "Low Pressure", "Low Pressure Average",
+                           "High Pressure", "High Pressure Average", 
+                           "Delta Pressure","Motor Current"]:
+                    data[key] = struct.unpack('f', wkrBytes)[0]
+                    
+            data_flag = False
+                    
+        except:
+            data_flag = True
+            print("Compressor output could not be converted to numbers."
+                  f"Skipping this data block. Bad output string is {rawdata}")
+           
+        return data_flag, data
     
     def __del__(self):
         """
@@ -104,11 +137,23 @@ class PTC:
         self.comm.close()
 
 class PTCAgent:
-    def __init__(self, agent, serial_mode, port, ip_address, f_sample=2.5):
+    """Agent to connect to a single cryomech compressor.
+
+    Args:
+        port (int): TCP port to connect to.
+        ip_address (str): IP Address for the compressor.
+        f_sample (float, optional): Data acquisiton rate
+        fake_errors (bool, optional): Generates fake errors in 
+        the string output 50% of the time. 
+
+    """
+    def __init__(self, agent, port, ip_address, f_sample=2.5, 
+                 fake_errors = False):
         self.agent = agent
         self.log = agent.log
         self.lock = TimeoutLock()
         self.ip_address = ip_address
+        self.fake_errors = fake_errors
 
         self.port = port
         self.module: Optional[Module] = None
@@ -146,7 +191,8 @@ class PTCAgent:
 
             session.set_status('starting')
             #Establish connection to ptc
-            self.ptc = PTC(self.ip_address, self.port)
+            self.ptc = PTC(self.ip_address, port=self.port, 
+                           fake_errors=self.fake_errors)
 
         self.initialized = True
 
@@ -167,21 +213,24 @@ class PTCAgent:
 
         with self.lock.acquire_timeout(0, job='acq') as acquired:
             if not acquired:
-                self.log.warn("Could not start acq because {} is already running"
-                              .format(self.lock.job))
+                self.log.warn("Could not start acq because {} is already" 
+                              "running".format(self.lock.job))
                 return False, "Could not acquire lock."
 
             session.set_status('running')
 
             self.take_data = True
             
-            #Publish data, waiting 1/f_sample seconds in between calls. 
+            #Publish data, waiting 1/f_sample seconds in between calls.
             while self.take_data:
                 pub_data = {'timestamp':time.time(), 'block_name':'ptc_status'}
-                data = self.ptc.get_data()
+                data_flag, data = self.ptc.get_data()
                 pub_data['data'] = data
+                #If there is an error in compressor output (data_flag = True), 
+                #do not publish
+                if not data_flag: 
+                    self.agent.publish_to_feed('ptc_status',pub_data)
                 time.sleep(1./self.f_sample)
-                self.agent.publish_to_feed('ptc_status',pub_data)
 
             self.agent.feeds["ptc_status"].flush_buffer() 
 
@@ -207,13 +256,14 @@ def main():
     pgroup.add_argument('--serial-number')
     pgroup.add_argument('--mode')
     pgroup.add_argument('--port')
+    pgroup.add_argument('--fake-errors', default=False)
     
     # Get the parser to process the command line.
     args = parser.parse_args()
 
     # Interpret options in the context of site_config.
     site_config.reparse_args(args, 'CryomechCPA1114Agent')
-    print('I am in charge of device with serial number: %s' % args.serial_number)
+    print('I am in charge of device with serial number: %s'%args.serial_number)
 
     # Automatically acquire data if requested (default)
     init_params = False
@@ -228,7 +278,8 @@ def main():
     agent, runner = ocs_agent.init_site_agent(args)
 
     #create agent instance and run log creation
-    ptc = PTCAgent(agent, 'modbus', args.port, args.ip_address)  
+    ptc = PTCAgent(agent, args.port, args.ip_address, 
+                   fake_errors=args.fake_errors)  
 
     agent.register_task('init',  ptc.init_ptc_task, startup=init_params)
     agent.register_process('acq', ptc.start_acq, ptc.stop_acq)
