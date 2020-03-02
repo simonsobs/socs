9cbad7710d6c27032e341842c3ba8d818a8e46ed
Brian Koopman <brian.koopman@yale.edu>
2020-03-02 14:39:14 -0500

    Initialize FrameRecorder in pytest fixture

--- tests/agents/test_timestream_aggregator.py
+++ tests/agents/test_timestream_aggregator.py
@@ -15,28 +15,23 @@ def frame_recorder(tmpdir):
 class TestReaderConnection():
     ## test successful connection (w/simulator in docker container)
     ## test unsucessfull connection (no simulator running)
-    def test_failed_g3reader_connection(self, tmpdir):
+    def test_failed_g3reader_connection(self, frame_recorder):
         """Without a backend setup this will fail, though we catch the
         RuntimeError, so it should pass anyway.
 
         """
-        p = tmpdir.mkdir("data")
-        record = FrameRecorder(10, "tcp://127.0.0.1:4536", p)
-        record._establish_reader_connection(timeout=1)
+        frame_recorder._establish_reader_connection(timeout=1)
 
     ## test rapid connection attempt (should take a second)
-    def test_rapid_reader_connection(self, tmpdir):
+    def test_rapid_reader_connection(self, frame_recorder):
         """Without a backend setup this will fail, though we catch the
         RuntimeError, so it should pass anyway.
 
         """
-        p = tmpdir.mkdir("data")
-        record = FrameRecorder(10, "tcp://127.0.0.1:4536", p)
-
         # Fake our last connection time, this should make the check of
         # t_diff < 1 true
-        record.last_connection_time = time.time()
-        record._establish_reader_connection(timeout=1)
+        frame_recorder.last_connection_time = time.time()
+        frame_recorder._establish_reader_connection(timeout=1)
 
 
 # test read_frames
@@ -50,9 +45,7 @@ class TestReaderConnection():
 
 # test split_acquisition
 def test_null_writer_on_split(frame_recorder):
-    yup = frame_recorder
-    print('HERE:', yup.data_dir)
-    pass
+    frame_recorder.split_acquisition()
 
 
 # test close_file
@@ -61,21 +54,15 @@ def test_null_writer_on_split(frame_recorder):
 ## Tests below here ##
 
 # test basic initialization
-def test_frame_recorder_init(tmpdir):
-    p = tmpdir.mkdir("data")
-    record = FrameRecorder(10, "tcp://127.0.0.1:4536", p)
+def test_frame_recorder_init(frame_recorder):
+    pass
 
 # test check_for_frame_gap
 ## test if writer is none we don't do anything
-def test_check_for_frame_gap(tmpdir):
-    p = tmpdir.mkdir("data")
-    record = FrameRecorder(10, "tcp://127.0.0.1:4536", p)
-    record.check_for_frame_gap()
+def test_check_for_frame_gap(frame_recorder):
+    frame_recorder.check_for_frame_gap()
 ## test we close file if acquisition split by gap size (need simulator running)
 
 # test create_new_file
-def test_create_new_file(tmpdir):
-    p = tmpdir.mkdir("data")
-    record = FrameRecorder(10, "tcp://127.0.0.1:4536", p)
-
-    record.create_new_file()
+def test_create_new_file(frame_recorder):
+    frame_recorder.create_new_file()
