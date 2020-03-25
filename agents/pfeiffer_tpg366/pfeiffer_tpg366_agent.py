8dcee970880543fda71c54378b7ebf16266243e7
lab1 <tanaybhandarkar@gmail.com>
2020-03-25 12:08:36 -0400

    Revisions to the Pfeiffer pressure monitor agent

--- agents/pfeiffer_tpg366/pfeiffer_tpg366_agent.py
+++ agents/pfeiffer_tpg366/pfeiffer_tpg366_agent.py
@@ -1,59 +1,63 @@
-#script to log and readout pfeiffer TPG 366 gauge contoller
-#via Ethernet connection
-#Zhilei Xu, Tanay Bhandarkar
+# Script to log and readout pfeiffer TPG 366 gauge contoller
+# via Ethernet connection
+# Zhilei Xu, Tanay Bhandarkar
 
 import socket
 import numpy as np
-from ocs import ocs_agent, site_config, client_t
+from ocs import ocs_agent, site_config
 from ocs.ocs_twisted import TimeoutLock
 import time
 
 BUFF_SIZE = 128
 ENQ = '\x05'
-IP_ADDRESS = '10.10.10.20'
+
 
 class pfeiffer:
-    """
-    CLASS to control and retrieve data from the pfeiffer tpg366 
+    """CLASS to control and retrieve data from the pfeiffer tpg366
     pressure gauage controller
-    ip_address: ip address of the deivce
-    port: 8000 (fixed for the device)
-    
+
+
+    Args:
+        ip_address: IP address of the deivce
+        porti (int): 8000 (fixed for the device)
+
     Attributes:
        read_pressure reads the pressure from one channel (given as an argument)
        read_pressure_all reads pressrue from the six channels
        close closes the socket
-    
+
     Todo:
         -Generalize IP address inputs (input IP  via command line?)
     """
-    def __init__(self, ip_address=IP_ADDRESS, port=8000, timeout=10, f_sample = 2.5):
+    def __init__(self, ip_address, port, timeout=10,
+                 f_sample=2.5):
         self.ip_address = ip_address
         self.port = port
         self.comm = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.comm.connect((self.ip_address, self.port))
         self.comm.settimeout(timeout)
-    
+
     def read_pressure(self, ch_no):
         """
         Function to measure the pressure of one given channel
         ch_no is the chanel to be measured (e.g. 1-6)
         returns the measured pressure as a float
-        
+
         Args:
             ch_no: The channel to be measured (1-6)
+
         Returns:
             pressure as a float
         """
-        msg = 'PR%d\r\n'%ch_no
+        msg = 'PR%d\r\n' % ch_no
         self.comm.send(msg.encode())
-        status = self.comm.recv(BUFF_SIZE).decode()#Could probably use this to catch exemptions
+        # Can use this to catch exemptions, for troubleshooting
+        status = self.comm.recv(BUFF_SIZE).decode()
         self.comm.send(ENQ.encode())
         read_str = self.comm.recv(BUFF_SIZE).decode()
         pressure_str = read_str.split(',')[-1].split('\r')[0]
         pressure = float(pressure_str)
-        #print(pressure)
-        return pressure        
+        return pressure
 
     def read_pressure_all(self):
         """measure the pressure of all channel
@@ -61,53 +65,53 @@ class pfeiffer:
 
         Args:
             None
+
         Returns:
-            6 element array corresponding to each channels pressure reading, as floats
+            6 element array corresponding to each channels
+            pressure reading, as floats
         """
         msg = 'PRX\r\n'
         self.comm.send(msg.encode())
-        status = self.comm.recv(BUFF_SIZE).decode()#Could probably use this to catch exemptions
+        # Could use this to catch exemptions, for troubleshooting
+        status = self.comm.recv(BUFF_SIZE).decode()
         self.comm.send(ENQ.encode())
         read_str = self.comm.recv(BUFF_SIZE).decode()
         pressure_str = read_str.split('\r')[0]
         gauge_states = pressure_str.split(',')[::2]
-        gauge_states = np.array(gauge_states, dtype=int)#this is recorded just incase
+        gauge_states = np.array(gauge_states, dtype=int)
         pressures = pressure_str.split(',')[1::2]
         pressures = [float(p) for p in pressures]
-        #print(pressures,type(pressures[0]),"This works")
-        return pressures 
+        return pressures
 
     def close(self):
-        """
-        Close the socket of the connection
-        """
+        """Close the socket of the connection"""
         self.comm.close()
 
+
 class pfeifferAgent:
-    def __init__(self, agent,ip_address = IP_ADDRESS, port=8000, f_sample=2.5):
-        self.active = True  
+
+    def __init__(self, agent, ip_address, port, f_sample=2.5):
+        self.active = True
         self.agent = agent
         self.log = agent.log
         self.lock = TimeoutLock()
         self.f_sample = f_sample
         self.take_data = False
-        self.module: Optional[Module] = None
-        self.gauge = pfeiffer()
+        self.gauge = pfeiffer(ip_address, int(port))
         self.port = port
-        self.ip_address = ip_address
-       
-        agg_params = {'frame length':60,}
+        agg_params = {'frame length': 60, }
 
         self.agent.register_feed('pressures',
                                  record=True,
                                  agg_params=agg_params,
                                  buffer_time=1)
-    
-    def start_acq(self,session, params = None):
+
+    def start_acq(self, session, params=None):
         """
         Get pressures from the Pfeiffer gauges, publishes them to the feed
-        
-        Args: 
+
+
+        Args:
             sampling_frequency- defaults to 2.5 Hz
 
         """
@@ -121,7 +125,7 @@ class pfeifferAgent:
         sleep_time = 1./f_sample - 0.01
 
         with self.lock.acquire_timeout(timeout=0, job='init') as acquired:
-        # Locking mechanism stops code from proceeding if no lock acquired
+            # Locking mechanism stops code from proceeding if no lock acquired
             if not acquired:
                 self.log.warn("Could not start init because {} is already running".format(self.lock.job))
                 return False, "Could not acquire lock."
@@ -137,18 +141,17 @@ class pfeifferAgent:
                     'data': {}
                 }
                 pressure_array = self.gauge.read_pressure_all()
-                #Loop through all the channels on the device
-                for i in range(len(pressure_array)):
-                    data['data']["pressure_ch"+str(i)]= pressure_array[i]
-                
-                self.agent.publish_to_feed('pressures',data)
+                # Loop through all the channels on the device
+                for channel in range(len(pressure_array)):
+                    data['data']["pressure_ch"+str(channel+1)] = pressure_array[channel]
+
+                self.agent.publish_to_feed('pressures', data)
                 time.sleep(sleep_time)
-            
+
             self.agents.feeds['pressures'].flush_buffer()
         return True, 'Acquistion exited cleanly'
 
-    
-    def stop_acq(self,session, params = None):
+    def stop_acq(self, session, params=None):
         """
         End pressure data acquisition
         """
@@ -160,20 +163,19 @@ class pfeifferAgent:
             return False, 'acq is not currently running'
 
 
-
 if __name__ == '__main__':
-        parser = site_config.add_arguments()
+    parser = site_config.add_arguments()
 
-        pgroup = parser.add_argument_group('Agent Options')
-        pgroup.add_argument('--ip_address')
-        pgroup.add_argument('--port')
+    pgroup = parser.add_argument_group('Agent Options')
+    pgroup.add_argument('--ip_address')
+    pgroup.add_argument('--port')
 
-        args = parser.parse_args() 
+    args = parser.parse_args()
 
-        site_config.reparse_args(args, 'pfeifferAgent')
-        agent, runner = ocs_agent.init_site_agent(args)
-        pfeiffer_agent = pfeifferAgent(agent) 
-        agent.register_process('acq', pfeiffer_agent.start_acq, pfeiffer_agent.stop_acq, startup = True)
-        agent.register_task('close', pfeiffer_agent.stop_acq)
-        agent.register_task('test', pfeiffer_agent.test_function)
-        runner.run(agent, auto_reconnect=True)
+    site_config.reparse_args(args, 'pfeifferAgent')
+    agent, runner = ocs_agent.init_site_agent(args)
+    pfeiffer_agent = pfeifferAgent(agent, args.ip_address, args.port)
+    agent.register_process('acq', pfeiffer_agent.start_acq,
+                           pfeiffer_agent.stop_acq, startup=True)
+    agent.register_task('close', pfeiffer_agent.stop_acq)
+    runner.run(agent, auto_reconnect=True)
