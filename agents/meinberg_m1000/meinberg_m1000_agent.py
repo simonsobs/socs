1594175a7ae34c5653a40477a2a76926fc885198
Brian Koopman <brian.koopman@yale.edu>
2020-08-27 18:02:42 +0000

    Move Meinberg interaction to MeinbergSNMP object
    Mostly this moves all the direct interaction with the Meinberg to a new object,
    MeinbergSNMP. This structure is similar to how several other Agents are
    structured. We also introduce a oid_cache, which is used to store the latest
    values of each OID and decoded strings for human readability. This cache is
    passed to the session.data object for inspection via OCS Clients.

--- agents/meinberg_m1000/meinberg_m1000_agent.py
+++ agents/meinberg_m1000/meinberg_m1000_agent.py
@@ -17,6 +17,214 @@ on_rtd = os.environ.get('READTHEDOCS') == 'True'
 if not on_rtd:
     from ocs import ocs_agent, site_config
 
+# Mapping of integer OID values to meaningful strings from M1000 manual
+REFCLOCKSTATE = {0: "refclock is not available",
+                 1: "synchronized",
+                 2: "not synchronized"}
+
+NTPCURRENTSTATE = {0: "not available",
+                   1: "not synchronized",
+                   2: "synchronized"}
+
+SYSPSSTATUS = {0: "notAvailable",
+               1: "down",
+               2: "up"}
+
+ETHPORTLINKSTATE = {0: "notAvailable",
+                    1: "up"}
+
+PTPPORTSTATE = {0: "uninitialized",
+                1: "initializing",
+                2: "faulty",
+                3: "disabled",
+                4: "listening",
+                5: "preMaster",
+                6: "master",
+                7: "passive",
+                8: "uncalibrated",
+                9: "slave"}
+
+
+class MeinbergSNMP:
+    """Meinberg SNMP communicator. Handles communication with and decoding of
+    values from the Meinberg M1000.
+
+    Parameters
+    ----------
+    address : str
+        Address of the M1000.
+    port : int
+        SNMP port to issue GETs to, default to 161.
+
+    Attributes
+    ----------
+    address : str
+        Address of the M1000.
+    port : int
+        SNMP port to issue GETs to.
+    snmp : socs.snmp.SNMPTwister
+        snmp handler from SOCS
+    decoder : dict
+        dict to facilitate the decoding of integer OID values returned from
+        queries to the M1000
+    mib_timings : list
+        list of dicts describing the SNMP OIDs to check, and at which
+        intervals. Each dict contains the keys "oid", "interval", and
+        "lastGet". The corresponding values are of types tuple, integer, and
+        float, respectively. "lastGet" is initialized as None, since no SNMP
+        GET commands have been issued.
+    oid_cache : dict
+        Cache of OID values and corresponding decoded values. Meant to pass to
+        session.data.
+
+    """
+    def __init__(self, address, port=161):
+        self.log = txaio.make_logger()
+        self.address = address
+        self.port = port
+        self.snmp = SNMPTwister(address, port)
+
+        # Decode OID States
+        self.decoder = {'mbgLtNgRefclockState': REFCLOCKSTATE,
+                        'mbgLtNgNtpCurrentState': NTPCURRENTSTATE,
+                        'mbgLtNgSysPsStatus': SYSPSSTATUS,
+                        'mbgLtNgEthPortLinkState': ETHPORTLINKSTATE,
+                        'mbgLtNgPtpPortState': PTPPORTSTATE}
+
+        # OIDs and how often to query them
+        self.mib_timings = [{"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgRefclockState', 1),
+                             "interval": 60,
+                             "lastGet": None},
+                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgSysPsStatus', 1),
+                             "interval": 60,
+                             "lastGet": None},
+                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgSysPsStatus', 2),
+                             "interval": 60,
+                             "lastGet": None},
+                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgEthPortLinkState', 1),
+                             "interval": 60,
+                             "lastGet": None},
+                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgRefclockLeapSecondDate', 1),
+                             "interval": 60*60,
+                             "lastGet": None},
+                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgNtpCurrentState', 0),
+                             "interval": 64,
+                             "lastGet": None},
+                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgPtpPortState', 1),
+                             "interval": 3,
+                             "lastGet": None}]
+
+        self.oid_cache = {}
+
+        # Determine unique interval values
+        self.interval_groups = list({x['interval'] for x in self.mib_timings})
+
+    def dump_cache(self):
+        """Return the current cache. Should be used to pass cached values to session.data.
+
+        """
+        return self.oid_cache
+
+    def build_get_list(self, interval):
+        """Create list of OIDs to GET based on last time we checked them.
+
+        Intervals are defined in the mib_timings dictionary. If the interval
+        time, or more, has passed since the last GET was issued, we add the OID to the
+        get_list.
+
+        Parameters
+        ----------
+        interval : int
+            Interval to build the get list for. Since available fields cannot
+            change dynamically within OCS we publish each interval group to its
+            own block.
+
+        Returns
+        -------
+        get_list : list
+            List of OID tuples to be passed to an SNMPTwister object in a GET call.
+
+        """
+        get_list = []
+        for mib in self.mib_timings:
+            # Select only OIDs of the same interval
+            if mib['interval'] != interval:
+                continue
+
+            if mib["lastGet"] is None:
+                get_list.append(mib["oid"])
+            elif time.time() - mib["lastGet"] > mib["interval"]:
+                get_list.append(mib["oid"])
+
+        return get_list
+
+    @inlineCallbacks
+    def get_intervals(self, session):
+        # Loop through interval groups and issue get commands for each
+        # interval group, publishing to a block with the interval in the
+        # name.
+        for interval in self.interval_groups:
+            # SNMP GET response
+            result = []
+
+            # Create list of OIDs to GET based on last time we checked them
+            get_list = self.build_get_list(interval)
+
+            # empty if an interval of time hasn't passed since last GET
+            if not get_list:
+                continue
+
+            # Issue SNMP GET command
+            result = yield self.snmp.get(get_list)
+            read_time = time.time()
+
+            message = {
+                'block_name': f'm1000_{interval}',
+                'timestamp': read_time,
+                'data': {}
+            }
+
+            for item in result:
+                # OID from SNMP GET
+                oid = item[0].prettyPrint()
+                # Makes something like 'MBG-SNMP-LTNG-MIB::mbgLtNgRefclockState.1'
+                # look like 'mbgLtNgRefclockState_1'
+                field_name = oid.split("::")[1].replace('.', '_')
+
+                # Grab OID value, mostly these are integers
+                oid_value = item[1]._value
+
+                # Decode string values
+                if isinstance(oid_value, bytes):
+                    oid_value = oid_value.decode("utf-8")
+
+                if not isinstance(oid_value, (int, bytes)):
+                    self.log.error("{oid} is of unknown and unhandled type " +
+                                   "{oid_type}. This OID will not be recorded.",
+                                   oid=oid, oid_type=type(oid_value))
+                    continue
+
+                self.log.debug("{o} {value}",
+                               o=oid,
+                               value=oid_value)
+                message['data'][field_name] = oid_value
+
+                # Update OID Cache for session.data
+                self.oid_cache[field_name] = {"status": oid_value}
+                oid_base_str = field_name.split('_')[0]
+                if oid_base_str in self.decoder:
+                    self.oid_cache[field_name]["description"] = self.decoder[oid_base_str][oid_value]
+                    self.oid_cache[field_name]["lastGet"] = read_time
+
+            # Update lastGet time
+            for mib in self.mib_timings:
+                if mib['oid'] in get_list:
+                    mib['lastGet'] = read_time
+
+            self.log.debug("{msg}", msg=message)
+            session.app.publish_to_feed('m1000', message)
+            session.data = self.dump_cache()
+
 
 class MeinbergM1000Agent:
     """Monitor the Meinberg LANTIME M1000 timing system via SNMP.
@@ -26,42 +234,27 @@ class MeinbergM1000Agent:
     agent : OCSAgent
         OCSAgent object which forms this Agent
     address : str
-        Address of the host sending the data via the G3NetworkSender
+        Address of the M1000.
     port : int
-        Port to listen for data on
+        SNMP port to issue GETs to, default to 161.
 
     Attributes
     ----------
     agent : OCSAgent
         OCSAgent object which forms this Agent
-    address : str
-        Address of the host sending the data via the G3NetworkSender.
-    port : int
-        Port to listen for data on.
     is_streaming : bool
-        Tracks whether or not the recorder is writing to disk. Setting to
-        false stops the recording of data.
+        Tracks whether or not the agent is actively issuing SNMP GET commands
+        to the M1000. Setting to false stops sending commands.
     log : txaio.tx.Logger
         txaio logger object, created by the OCSAgent
-    snmp : socs.snmp.SNMPTwister
-        snmp handler from SOCS
-    mib_timings : list
-        list of dicts describing the SNMP OIDs to check, and at which
-        intervals. Each dict contains the keys "oid", "interval", and
-        "lastGet". The corresponding values are of types tuple, integer, and
-        float, respectively. "lastGet" is initialized as None, since no SNMP
-        GET commands have been issued.
 
     """
-
     def __init__(self, agent, address, port=161):
         self.agent = agent
-        self.address = address
-        self.port = port
         self.is_streaming = False
         self.log = self.agent.log
 
-        self.snmp = SNMPTwister(address, port)
+        self.meinberg = MeinbergSNMP(address, port)
 
         agg_params = {
             'frame_length': 10*60  # [sec]
@@ -71,19 +264,43 @@ class MeinbergM1000Agent:
                                  agg_params=agg_params,
                                  buffer_time=1)
 
-        self.mib_timings = [{"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgRefclockState', 1), "interval": 60, "lastGet": None},
-                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgSysPsStatus', 1), "interval": 60, "lastGet": None},
-                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgSysPsStatus', 2), "interval": 60, "lastGet": None},
-                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgEthPortLinkState', 1), "interval": 60, "lastGet": None},
-                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgRefclockLeapSecondDate', 1), "interval": 60*60, "lastGet": None},
-                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgNtpCurrentState', 0), "interval": 64, "lastGet": None},
-                            {"oid": ('MBG-SNMP-LTNG-MIB', 'mbgLtNgPtpPortState', 1), "interval": 3, "lastGet": None}]
-
     @inlineCallbacks
     def start_record(self, session, params=None):
         """start_record(params=None)
 
         OCS Process for fetching values from the M1000 via SNMP.
+
+        The session.data object stores each unique OID with its latest status,
+        decoded value, and the last time the value was retrived. This will look like
+        the example here::
+
+            >>> session.data
+            {"mbgLtNgPtpPortState_1":
+                {"status": 3,
+                 "description": "disabled",
+                 "lastGet": 1598543397.689727},
+            "mbgLtNgNtpCurrentState_0":
+                {"status": 1,
+                 "description": "not synchronized",
+                 "lastGet": 1598543363.289597},
+            "mbgLtNgRefclockState_1":
+                {"status": 2,
+                 "description": "not synchronized",
+                 "lastGet": 1598543359.6326838},
+            "mbgLtNgSysPsStatus_1":
+                {"status": 2,
+                 "description": "up",
+                 "lastGet": 1598543359.6326838},
+            "mbgLtNgSysPsStatus_2":
+                {"status": 2,
+                 "description": "up",
+                 "lastGet": 1598543359.6326838},
+            "mbgLtNgEthPortLinkState_1":
+                {"status": 1,
+                 "description": "up",
+                 "lastGet": 1598543359.6326838}}
+
+        Note that session.data is populated within the self.meinberg.get_intervals() call.
 
         """
         if params is None:
@@ -91,66 +308,10 @@ class MeinbergM1000Agent:
 
         self.is_streaming = True
 
-        # Determine unique interval values
-        interval_groups = list({x['interval'] for x in self.mib_timings})
-
         while self.is_streaming:
-            # Loop through interval groups and issue get commands for each
-            # interval group, publishing to a block with the interval in the
-            # name.
-            for interval in interval_groups:
-                # SNMP GET response
-                result = []
-
-                # Create list of OIDs to GET based on last time we checked them
-                get_list = []
-                for mib in self.mib_timings:
-                    # Select only OIDs of the same interval
-                    if mib['interval'] != interval:
-                        continue
-
-                    if mib["lastGet"] is None:
-                        get_list.append(mib["oid"])
-                    elif time.time() - mib["lastGet"] > mib["interval"]:
-                        get_list.append(mib["oid"])
-
-                if get_list:
-                    # Issue SNMP GET command
-                    result = yield self.snmp.get(get_list)
-                    read_time = time.time()
-
-                if result:
-                    message = {
-                        'block_name': f'm1000_{interval}',
-                        'timestamp': read_time,
-                        'data': {}
-                    }
-
-                    for item in result:
-                        try:
-                            # OID from SNMP GET
-                            oid = item[0].prettyPrint()
-                            # Makes something like 'MBG-SNMP-LTNG-MIB::mbgLtNgRefclockState.1'
-                            # look like 'mbgLtNgRefclockState_1'
-                            valid_field = oid.split("::")[1].replace('.', '_')
-
-                            self.log.debug("{o} {value}",
-                                           o=oid,
-                                           value=int(item[1]))
-                            message['data'][valid_field] = int(item[1])
-                        except ValueError:
-                            self.log.warn('{oid} is of type {_type}, not int',
-                                          oid=item[1], _type=type(item[1]))
-
-                    # Update lastGet time
-                    for mib in self.mib_timings:
-                        if mib['oid'] in get_list:
-                            mib['lastGet'] = read_time
-
-                    self.log.debug("{msg}", msg=message)
-                    session.app.publish_to_feed('m1000', message)
-
-                yield dsleep(0.1)
+            yield self.meinberg.get_intervals(session)
+            self.log.debug("{data}", data=session.data)
+            yield dsleep(0.1)
 
         return True, "Finished Recording"
 
