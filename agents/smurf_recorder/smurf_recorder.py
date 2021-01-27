e7a700a59a57a12cf121fe6f92e01449462bbdcb
jlashner <jlashner@gmail.com>
2021-01-26 21:48:54 -0500

    Adds set_status calls to some operations

--- agents/smurf_recorder/smurf_recorder.py
+++ agents/smurf_recorder/smurf_recorder.py
@@ -136,6 +136,7 @@ class SmurfRecorder:
                                  self.data_dir, self.stream_id,
                                  target_rate=self.target_rate)
 
+        session.set_status('running')
         while self.is_streaming:
             recorder.monitored_channels = self.monitored_channels
             recorder.target_rate = self.target_rate
@@ -156,6 +157,7 @@ class SmurfRecorder:
 
         """
         self.is_streaming = False
+        session.set_status('stopping')
         return True, "Stopping Recording"
 
 
