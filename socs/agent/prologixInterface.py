be7e64606f8e40f0694e6d7ccd492e3f44a85de5
Brian Koopman <bjk98@cornell.edu>
2021-02-18 11:46:38 -0500

    Base GpibInterface on PrologixInterface class

--- socs/agent/prologixInterface.py
+++ socs/agent/prologixInterface.py
@@ -3,31 +3,10 @@ import socket
 DEFAULT_ESCAPE = 'xYzZyX'
 
 
-class GpibInterface:
-    def __init__(self, ip_address, gpibAddr):
-        self.pro = PrologixInterface(ip=ip_address)
-        self.gpibAddr = gpibAddr
-
-    def connGpib(self):
-        self.pro.write('++addr ' + str(self.gpibAddr))
-
-    def write(self, msg):
-        self.connGpib()
-        self.pro.write(msg)
-
-    def read(self):
-        return self.pro.read()
-
-    def identify(self):
-        self.write('*idn?')
-        return self.read()
-
-
 class PrologixInterface:
     def __init__(self, ip, escape_string=DEFAULT_ESCAPE):
         self.ip = ip
         self.escape_string = escape_string
-        # self.gpibAddr = gpibAddr
         self.sock = None
         self.connSocket()
         self.configure()
@@ -40,19 +19,28 @@ class PrologixInterface:
     def configure(self):
         self.write('++mode 1\n')
         self.write('++auto 1\n')
-        # self.write('++addr ' + str(self.gpibAddr))
 
     def write(self, msg):
         message = msg + '\n'
         self.sock.send(message.encode())
-
-    # def writeGpib(self, gpibAddr, msg):
-    # 	self.write('++addr ' + str(gpibAddr))
-    # 	self.write(msg)
 
     def read(self):
         return self.sock.recv(128).decode().rstrip('\n').rstrip('\r')
 
     def identify(self):
         self.write('++ver')
+        return self.read()
+
+
+class GpibInterface(PrologixInterface):
+    def __init__(self, ip_address, gpibAddr):
+        super().__init__(ip_address)
+        self.gpibAddr = gpibAddr
+
+    def write(self, msg):
+        self.sock.write('++addr ' + str(self.gpibAddr))
+        super().write(msg)
+
+    def identify(self):
+        self.write('*idn?')
         return self.read()
