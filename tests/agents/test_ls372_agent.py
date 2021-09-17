e4a490d58a76fcc9b26d7bf95b7f017e5b696378
Brian Koopman <brian.koopman@yale.edu>
2021-09-17 12:00:42 -0400

    Test init_lakeshore_task

--- tests/agents/test_ls372_agent.py
+++ tests/agents/test_ls372_agent.py
@@ -10,21 +10,23 @@ from unittest import mock
 import txaio
 txaio.use_twisted()
 
-# Mocks and fixtures
-#def mock_connection_send_recv():
-#    mock_con = mock.Mock()
-#    mock_con.send = lambda s: len(s.encode('utf-8'))
-#    mock_con.recv = mock.Mock()
-#
-#    values = {'*IDN?': 'LSCI,MODEL372,LSA23JD,1.3'}
-#    def side_effect(arg):
-#        return values[arg]
-#    mock_con.recv.side_effect = side_effect
-#
-#    return mock_con
 
+# Mocks and fixtures
 def mock_connection():
+    """Mock a standard connection."""
     return mock.MagicMock()
+
+
+def mock_failed_connection():
+    """Mock an unhandled error during connection.
+
+    Typically a failed connection will raise a ConnectionError, but we want to
+    check unhandled exceptions in the tests too.
+
+    """
+    failed_con = mock.Mock(side_effect=RuntimeError('Unhandled error'))
+    return failed_con
+
 
 @pytest.fixture
 def mock_agent():
@@ -37,50 +39,117 @@ def mock_agent():
 
     return mock_agent
 
+
 # Mock responses from the 372
-mock_msg = mock.Mock()
+def mock_372_msg():
+    mock_msg = mock.Mock()
 
-values = {'*IDN?': 'LSCI,MODEL372,LSA23JD,1.3',
-          'SCAN?': '01,1',
-          'INSET? A': '0,010,003,00,1',
-          'INNAME? A': 'Input A',
-          'INTYPE? A': '1,04,0,15,0,2',
-          'TLIMIT? A': '+0000',
-          'OUTMODE? 0': '2,6,1,0,0,001',
-          'HTRSET? 0': '+120.000,8,+0000.00,1',
-          'OUTMODE? 2': '4,16,1,0,0,001',
-          'HTRSET? 2': '+120.000,8,+0000.00,1',
-}
+    values = {'*IDN?': 'LSCI,MODEL372,LSA23JD,1.3',
+              'SCAN?': '01,1',
+              'INSET? A': '0,010,003,00,1',
+              'INNAME? A': 'Input A',
+              'INTYPE? A': '1,04,0,15,0,2',
+              'TLIMIT? A': '+0000',
+              'OUTMODE? 0': '2,6,1,0,0,001',
+              'HTRSET? 0': '+120.000,8,+0000.00,1',
+              'OUTMODE? 2': '4,16,1,0,0,001',
+              'HTRSET? 2': '+120.000,8,+0000.00,1'}
 
-for i in range(1, 17):
-    values[f'INSET? {i}'] = '1,007,003,21,1'
-    values[f'INNAME? {i}'] = f'Channel {i:02}'
-    values[f'INTYPE? {i}'] = '0,07,1,10,0,1'
-    values[f'TLIMIT? {i}'] = '+0000'
+    for i in range(1, 17):
+        values[f'INSET? {i}'] = '1,007,003,21,1'
+        values[f'INNAME? {i}'] = f'Channel {i:02}'
+        values[f'INTYPE? {i}'] = '0,07,1,10,0,1'
+        values[f'TLIMIT? {i}'] = '+0000'
 
-def side_effect(arg):
-    return values[arg]
+    def side_effect(arg):
+        return values[arg]
 
-mock_msg.side_effect = side_effect
+    mock_msg.side_effect = side_effect
+
+    return mock_msg
 
 
 # Tests
-def test_ls372(mock_agent):
-    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1' )
-    return agent
-
 @mock.patch('socs.Lakeshore.Lakeshore372._establish_socket_connection', mock_connection())
-@mock.patch('socs.Lakeshore.Lakeshore372.LS372.msg', mock_msg)
+@mock.patch('socs.Lakeshore.Lakeshore372.LS372.msg', mock_372_msg())
 def test_ls372_init_lakeshore_task(mock_agent):
-    #mock_agent = mock.MagicMock()
-    #log = txaio.make_logger()
-    #txaio.start_logging(level='debug')
-    #mock_agent.log = log
+    """Run init_lakeshore_task, mocking a connection and the 372 messaging.
+    This should be as if the initialization worked without issue.
 
-    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1' )
+    """
+    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1')
 
     mock_app = mock.MagicMock()
     session = OpSession(1, 'init_lakeshore', app=mock_app)
-    response = agent.init_lakeshore_task(session, None)
-    print(response)
-    return response
+    res = agent.init_lakeshore_task(session, None)
+
+    print(res)
+    print(agent.initialized)
+    print(session.encoded())
+    assert agent.initialized is True
+    assert res[0] is True
+
+
+@mock.patch('socs.Lakeshore.Lakeshore372._establish_socket_connection', mock_connection())
+@mock.patch('socs.Lakeshore.Lakeshore372.LS372.msg', mock_372_msg())
+def test_ls372_init_lakeshore_task_already_initialized(mock_agent):
+    """Initializing an already initialized LS372_Agent should just return True.
+
+    """
+    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1')
+
+    mock_app = mock.MagicMock()
+    session = OpSession(1, 'init_lakeshore', app=mock_app)
+    agent.init_lakeshore_task(session, None)
+    res = agent.init_lakeshore_task(session, None)
+    assert agent.initialized is True
+    assert res[0] is True
+
+
+# If we don't patch the reactor out, it'll mess up pytest when stop is called
+@mock.patch('LS372_agent.reactor', mock.MagicMock())
+@mock.patch('socs.Lakeshore.Lakeshore372.LS372.msg', mock_372_msg())
+def test_ls372_init_lakeshore_task_failed_connection(mock_agent):
+    """Leaving off the connection Mock, if the connection fails the init task
+    should fail and return False.
+
+    """
+    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1')
+
+    mock_app = mock.MagicMock()
+    session = OpSession(1, 'init_lakeshore', app=mock_app)
+    res = agent.init_lakeshore_task(session, None)
+    assert res[0] is False
+
+
+# If we don't patch the reactor out, it'll mess up pytest when stop is called
+@mock.patch('LS372_agent.reactor', mock.MagicMock())
+@mock.patch('socs.Lakeshore.Lakeshore372._establish_socket_connection', mock_failed_connection())
+@mock.patch('socs.Lakeshore.Lakeshore372.LS372.msg', mock_372_msg())
+def test_ls372_init_lakeshore_task_unhandled_error(mock_agent):
+    """If we cause an unhandled exception during connection init task should
+    also fail and return False.
+
+    """
+    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1')
+
+    mock_app = mock.MagicMock()
+    session = OpSession(1, 'init_lakeshore', app=mock_app)
+    res = agent.init_lakeshore_task(session, None)
+    assert res[0] is False
+
+
+@mock.patch('socs.Lakeshore.Lakeshore372._establish_socket_connection', mock_connection())
+@mock.patch('socs.Lakeshore.Lakeshore372.LS372.msg', mock_372_msg())
+def test_ls372_init_lakeshore_task_auto_acquire(mock_agent):
+    """If we initalize and pass the auto_acquire param, we should expect the
+    Agent to make a start call for the acq process, given the params in acq_params.
+
+    """
+    agent = LS372_Agent(mock_agent, 'mock372', '127.0.0.1')
+
+    mock_app = mock.MagicMock()
+    session = OpSession(1, 'init_lakeshore', app=mock_app)
+    res = agent.init_lakeshore_task(session, {'auto_acquire': True, 'acq_params': {'test': 1}})
+    assert res[0] is True
+    agent.agent.start.assert_called_once_with('acq', {'test': 1})
