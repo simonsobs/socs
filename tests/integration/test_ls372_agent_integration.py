dd64a67e6a890cba86458beaf5b33352280c30d2
Brian Koopman <brian.koopman@yale.edu>
2021-09-23 18:06:44 -0400

    Add integration test example with working coverage

--- tests/integration/test_ls372_agent_integration.py
+++ tests/integration/test_ls372_agent_integration.py
@@ -1,8 +1,14 @@
+import os
 import time
 import pytest
+import signal
+import subprocess
+import coverage.data
 import urllib.request
 
 from urllib.error import URLError
+
+from ocs.matched_client import MatchedClient
 
 pytest_plugins = ("docker_compose")
 
@@ -24,6 +30,38 @@ def wait_for_crossbar(function_scoped_container_getter):
     assert code == 200
     print("Crossbar server online.")
 
+@pytest.fixture()
+def run_agent(cov):
+    env = os.environ.copy()
+    env['COVERAGE_FILE'] = '.coverage.agent'
+    env['OCS_CONFIG_DIR'] = os.getcwd()
+    agentproc = subprocess.Popen(['coverage', 'run', '--rcfile=../.coveragerc', '../agents/lakeshore372/LS372_agent.py', '--site-file', './default.yaml'],
+                                 env=env,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE,
+                                 preexec_fn=os.setsid)
+
+    # wait for Agent to initialize (can we make this faster?)
+    time.sleep(10)
+
+    yield
+
+    agentproc.send_signal(signal.SIGINT)
+    time.sleep(1)
+    # 4.
+    agentcov = coverage.data.CoverageData(basename='.coverage.agent')
+    agentcov.read()
+    cov.get_data().update(agentcov)
+
 @pytest.mark.integtest
-def test_ls372_simulator(wait_for_crossbar):
+def test_testing(wait_for_crossbar):
+    """Just a quick test to make sure we can bring up crossbar."""
     assert True
+
+@pytest.mark.integtest
+def test_agent(wait_for_crossbar, run_agent):
+    os.environ['OCS_CONFIG_DIR'] = os.getcwd()
+    print(os.getenv('OCS_CONFIG_DIR'))
+    client = MatchedClient('LSASIM')
+    resp = client.init_lakeshore()
+    print(resp)
