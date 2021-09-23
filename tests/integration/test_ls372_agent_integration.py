c36d73a5e3fc1f709785796f1d8ac26663d6d5ad
Brian Koopman <brian.koopman@yale.edu>
2021-09-23 11:51:20 -0400

    Setup basis for docker-compose based integration testing

---
+++ tests/integration/test_ls372_agent_integration.py
@@ -0,0 +1,29 @@
+import time
+import pytest
+import urllib.request
+
+from urllib.error import URLError
+
+pytest_plugins = ("docker_compose")
+
+# Fixture to wait for crossbar server to be available.
+@pytest.fixture(scope="function")
+def wait_for_crossbar(function_scoped_container_getter):
+    """Wait for the crossbar server from docker-compose to become responsive."""
+    attempts = 0
+
+    while attempts < 6:
+        try:
+            code = urllib.request.urlopen("http://localhost:8001/info").getcode()
+        except (URLError, ConnectionResetError):
+            print("Crossbar server not online yet, waiting 5 seconds.")
+            time.sleep(5)
+
+        attempts += 1
+
+    assert code == 200
+    print("Crossbar server online.")
+
+@pytest.mark.integtest
+def test_ls372_simulator(wait_for_crossbar):
+    assert True
