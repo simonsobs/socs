05d1f0117123429abba061286c9f1d79350e1e97
Shunsuke Adachi <sadachi5@gmail.com>
2022-01-05 14:06:29 +0900

    wiregrid_kikusui:modify style of src/common.py

--- agents/wiregrid_kikusui/src/common.py
+++ agents/wiregrid_kikusui/src/common.py
@@ -2,38 +2,34 @@ import os
 import time
 from datetime import datetime, timezone
 
+
 def openlog(log_dir):
     now = datetime.now(timezone.utc)
     dateStr = now.strftime('%Y-%m-%d')
 
     logfilename = '{}/OCS_{}.dat'.format(log_dir, dateStr)
-    if not os.path.isdir(log_dir): os.mkdir(log_dir)
+    if not os.path.isdir(log_dir):
+        os.mkdir(log_dir)
     if os.path.exists(logfilename):
         logfile = open(logfilename, 'a+')
     else:
         logfile = open(logfilename, 'w')
         log = '#UnixTime ON/OFF time-period[sec] position[def] mode\n'
         logfile.write(log)
-<<<<<<< HEAD
         logfile.flush()
-        pass
-=======
->>>>>>> 413645f62934adc2981a1bbcf19c8d99469998e3
     return logfile
 
-def writelog(logfile, onoff, timeperiod=0., position=0., mode='stepping_rot'):# calibration, continuous_rot
+
+# for calibration_wg() in kikusui_agent.py
+def writelog(logfile, onoff, timeperiod=0., position=0., mode='stepping_rot'):
     if timeperiod > 0.:
-<<<<<<< HEAD
-        log = '{:.6f} {:3s} {:8.3f} {:8.3f} {:14s}\n'.format(time.time(), onoff, timeperiod, position, mode)
-        pass
-=======
-        log = '{:.6f} {:3s} {:8.3f} {:8.3f}\n'.format(time.time(), onoff, timeperiod, position)
->>>>>>> 413645f62934adc2981a1bbcf19c8d99469998e3
+        log = '{:.6f} {:3s} {:8.3f} {:8.3f} {:14s}\n'\
+              .format(time.time(), onoff, timeperiod, position, mode)
+        log = '{:.6f} {:3s} {:8.3f} {:8.3f}\n'\
+              .format(time.time(), onoff, timeperiod, position)
     else:
-        log = '{:.6f} {:3s} {:8s} {:8.3f} {:14s}\n'.format(time.time(), onoff, '--------', position, mode)
+        log = '{:.6f} {:3s} {:8s} {:8.3f} {:14s}\n'\
+              .format(time.time(), onoff, '--------', position, mode)
     logfile.write(log)
-<<<<<<< HEAD
     logfile.flush()
-    pass
-=======
->>>>>>> 413645f62934adc2981a1bbcf19c8d99469998e3
+    return True
