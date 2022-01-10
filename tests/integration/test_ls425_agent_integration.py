72c5dcd9027cbe4b3d639fc9c3c8eed06874d4d4
Brian Koopman <brian.koopman@yale.edu>
2022-01-10 15:45:39 -0500

    Add copy of upstream integration/util.py
    These functions are very useful for testing, but aren't packaged within ocs at
    the moment. Probably that should be done, but I need them now for test
    development. Will move the issue upstream and come back to replace these.
    
    When coming back, the local util.py should likely be entirely replaced with the
    import from ocs, and the import lines here will need updating.

--- tests/integration/test_ls425_agent_integration.py
+++ tests/integration/test_ls425_agent_integration.py
@@ -13,76 +13,18 @@ from ocs.matched_client import MatchedClient
 import ocs
 from ocs.base import OpCode
 
+from integration.util import (
+    create_agent_runner_fixture,
+    create_client_fixture,
+    create_crossbar_fixture
+)
+
 pytest_plugins = ("docker_compose")
 
-# Set the OCS_CONFIG_DIR so we read the local default.yaml file always
-os.environ['OCS_CONFIG_DIR'] = os.getcwd()
-
-
-# Fixture to wait for crossbar server to be available.
-# Speeds up tests a bit to have this session scoped
-# If tests start interfering with one another this should be changed to
-# "function" scoped and session_scoped_container_getter should be changed to
-# function_scoped_container_getter
-@pytest.fixture(scope="session")
-def wait_for_crossbar(session_scoped_container_getter):
-    """Wait for the crossbar server from docker-compose to become
-    responsive.
-
-    """
-    attempts = 0
-
-    while attempts < 6:
-        try:
-            code = urllib.request.urlopen("http://localhost:8001/info").getcode()
-        except (URLError, ConnectionResetError):
-            print("Crossbar server not online yet, waiting 5 seconds.")
-            time.sleep(5)
-
-        attempts += 1
-
-    assert code == 200
-    print("Crossbar server online.")
-
-
-@pytest.fixture()
-def run_agent(cov):
-    env = os.environ.copy()
-    env['COVERAGE_FILE'] = '.coverage.agent'
-    env['OCS_CONFIG_DIR'] = os.getcwd()
-    agentproc = subprocess.Popen(['coverage', 'run',
-                                  '--rcfile=./.coveragerc',
-                                  '../agents/lakeshore425/LS425_agent.py',
-                                  '--site-file',
-                                  './default.yaml'],
-                                 env=env,
-                                 stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE,
-                                 preexec_fn=os.setsid)
-
-    # wait for Agent to connect
-    time.sleep(1)
-
-    yield
-
-    # shutdown Agent
-    agentproc.send_signal(signal.SIGINT)
-    time.sleep(1)
-
-    # report coverage
-    agentcov = coverage.data.CoverageData(basename='.coverage.agent')
-    agentcov.read()
-    try:
-        cov.get_data().update(agentcov)
-    except AttributeError as e:
-        print(f"Encountered error reporting coverage: {e}")
-        print("Did you forget to throw the '--cov' flag?")
-
-
-@pytest.fixture()
-def client():
-    client = MatchedClient('LS425')
-    return client
+wait_for_crossbar = create_crossbar_fixture()
+run_agent = create_agent_runner_fixture(
+    '../agents/lakeshore425/LS425_agent.py', 'ls425_agent')
+client = create_client_fixture('LS425')
 
 
 @pytest.mark.integtest
