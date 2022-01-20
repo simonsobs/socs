0b2007c658a0ee726d2f48747e7959d143415a7d
Brian Koopman <brian.koopman@yale.edu>
2022-01-20 10:27:20 -0500

    Remove unused restart_crossbar function

--- tests/integration/util.py
+++ tests/integration/util.py
@@ -1,18 +1,14 @@
-import time
 import pytest
-import urllib.request
-import docker
-
-from urllib.error import URLError
 
 from ocs.testing import check_crossbar_connection
+
 
 def create_crossbar_fixture():
     # Fixture to wait for crossbar server to be available.
     # Speeds up tests a bit to have this session scoped
     # If tests start interfering with one another this should be changed to
-    # "function" scoped and session_scoped_container_getter should be changed to
-    # function_scoped_container_getter
+    # "function" scoped and session_scoped_container_getter should be changed
+    # to function_scoped_container_getter
     # @pytest.fixture(scope="function")
     # def wait_for_crossbar(function_scoped_container_getter):
     @pytest.fixture(scope="session")
@@ -24,11 +20,3 @@ def create_crossbar_fixture():
         check_crossbar_connection()
 
     return wait_for_crossbar
-
-
-def restart_crossbar():
-    """Restart the crossbar server and wait for it to come back online."""
-    client = docker.from_env()
-    crossbar_container = client.containers.get('ocs-tests-crossbar')
-    crossbar_container.restart()
-    check_crossbar_connection()
