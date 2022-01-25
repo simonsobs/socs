77120531017a5f4dac2905b4ea96d36efd961a2b
Brian Koopman <brian.koopman@yale.edu>
2022-01-25 14:10:31 -0500

    Remove spt3g dependent Agents

--- tests/agents/test_pysmurf_monitor.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, '../agents/pysmurf_monitor/')
-from pysmurf_monitor import PysmurfMonitor
--- tests/agents/test_smurf_stream_simulator.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.insert(0, '../agents/smurf_stream_simulator/')
-from smurf_stream_simulator import SmurfStreamSimulator
