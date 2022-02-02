608ac6d77236143f24d025eabb2603a18b1ea66a
Brian Koopman <brian.koopman@yale.edu>
2022-02-02 17:30:43 -0500

    Setup for building wheels
    A few things in this:
    - Update README to have absolute links, rather than relative links for resolving on PyPI
    - Cleanup requirements, getting rid of those pulled in by the ocs dependency
    - Move testing requirements to separate requirements file (though still call recursively in old file)
    - Fill out more info in setup.py and add pyproject.toml for builds

--- setup.py
+++ setup.py
@@ -2,9 +2,35 @@ from setuptools import setup, find_packages
 
 import versioneer
 
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name = 'socs',
+      long_description=long_description,
+      long_description_content_type="text/x-rst",
       version = versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       description = 'Simons Observatory Control System',
       package_dir = {'socs': 'socs'},
-      packages = find_packages())
+      packages = find_packages(),
+      url="https://github.com/simonsobs/socs",
+      project_urls={
+          "Source Code": "https://github.com/simonsobs/ocs",
+          "Documentation": "https://ocs.readthedocs.io/",
+          "Bug Tracker": "https://github.com/simonsobs/ocs/issues",
+      },
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Astronomy",
+          "Framework :: Twisted",
+      ],
+      python_requires=">=3.6",
+      install_requires=[
+          'ocs',
+          'pyserial',
+          'sqlalchemy',
+          'pysnmp',
+      ],
+)
