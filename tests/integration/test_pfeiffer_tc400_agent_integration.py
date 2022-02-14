9952070a843e8d76cefa9837c73231f110ad6b7e
Brian Koopman <brian.koopman@yale.edu>
2022-02-14 14:34:38 -0500

    Simplify creating TC400 response telegram

--- tests/integration/test_pfeiffer_tc400_agent_integration.py
+++ tests/integration/test_pfeiffer_tc400_agent_integration.py
@@ -21,6 +21,32 @@ pytest_plugins = ("docker_compose")
 # Set the OCS_CONFIG_DIR so we read the local default.yaml file always
 os.environ['OCS_CONFIG_DIR'] = os.getcwd()
 
+
+def chksum_msg(msg):
+    """Create and append the checksum ot a message."""
+    msg += "{:03d}\r".format(sum([ord(x) for x in msg])%256)
+    return msg
+
+
+def format_reply(data):
+    """Produce full message string we expect back from the TC400, provided only
+    the data segment.
+
+    The driver code reads the response, and only inspects the data section of
+    the telegram. This function is meant to make preparing the responses a bit
+    easier, by putting in something somewhat sensible for the rest of the telegram.
+
+    Parameters:
+        data (str): Data string to package into telegram.
+
+    Returns:
+        str: The full telegram string that is emulating the response from the
+            TC400.
+
+    """
+    return chksum_msg('001' + '10' + '010' + '{:02d}'.format(len(data)) + data)
+
+
 wait_for_crossbar = create_crossbar_fixture()
 run_agent = create_agent_runner_fixture(
     '../agents/pfeiffer_tc400/pfeiffer_tc400_agent.py', 'tc400_agent', args=['--log-dir', './logs'])
@@ -80,18 +106,12 @@ def test_pfeiffer_tc400_init_lakeshore(wait_for_crossbar, emulator, run_agent, c
 #    assert resp.session['op_code'] in [OpCode.STOPPING.value,
 #                                       OpCode.SUCCEEDED.value]
 
-def chksum_msg(msg):
-    """Create and append the checksum ot a message."""
-    msg += "{:03d}\r".format(sum([ord(x) for x in msg])%256)
-    return msg
-
 @pytest.mark.integtest
 def test_pfeiffer_tc400_turn_turbo_on(wait_for_crossbar, emulator, run_agent, client):
     client.init()
 
-    #responses = {'001 10 010 06 111111 015': '00110'}
-    responses = {'0011001006111111015': chksum_msg('0011001006111111'),  # ready_turbo()
-                 '0011002306111111019': chksum_msg('0011001006000000'),  # turn_turbo_motor_on()
+    responses = {'0011001006111111015': format_reply('111111'),  # ready_turbo()
+                 '0011002306111111019': format_reply('111111'),  # turn_turbo_motor_on()
                  #'0011002306111111019': '0011001006111111015\r',  # turn_turbo_motor_on()
     }
     emulator.define_responses(responses)
