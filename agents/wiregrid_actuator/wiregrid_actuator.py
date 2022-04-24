ab7353ce6e9f1869d0e9dd10086bea44984f5926
Shunsuke Adachi <sadachi5@gmail.com>
2022-04-25 00:14:26 +0900

    remove speedrate check in insert_test() and eject_test() becuase there are checks in param decorator means in wiregrid_actuator.py

--- agents/wiregrid_actuator/wiregrid_actuator.py
+++ agents/wiregrid_actuator/wiregrid_actuator.py
@@ -476,11 +476,6 @@ class WiregridActuatorAgent:
         # Get parameters
         distance = params.get('distance')
         speedrate = params.get('speedrate')
-        if speedrate < 0. or speedrate < 1.0:
-            self.log.warn('insert_test(): speedrate is out of range ({}).'
-                          'It should be in 0.0--1.0.'
-                          '--> Set to 0.2')
-            speedrate = 0.2
         self.log.info('insert_test(): set distance   = {} mm'
                       .format(distance))
         self.log.info('insert_test(): set speed rate = {}'
@@ -533,11 +528,6 @@ class WiregridActuatorAgent:
         # Get parameters
         distance = params.get('distance', 10)
         speedrate = params.get('speedrate', 0.2)
-        if speedrate < 0. or speedrate < 1.0:
-            self.log.warn('eject_test(): speedrate is out of range ({}).'
-                          'It should be in 0.0--1.0.'
-                          '--> Set to 0.2')
-            speedrate = 0.2
         self.log.info('eject_test(): set distance   = {} mm'
                       .format(distance))
         self.log.info('eject_test(): set speed rate = {}'
