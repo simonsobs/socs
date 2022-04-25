090e7e146c938165e17398cad6a45b66f3e0fe08
Jack Lashner <jlashner@gmail.com>
2022-04-25 15:46:17 -0400

    more realistic smurf-file-emulator g3 files

--- tests/agents/test_smurf_file_emulator.py
+++ tests/agents/test_smurf_file_emulator.py
@@ -2,7 +2,8 @@ import sys
 sys.path.insert(0, '../agents/smurf_file_emulator/')
 import os
 from argparse import Namespace
-from smurf_file_emulator import SmurfFileEmulator
+from smurf_file_emulator import SmurfFileEmulator, make_parser
+
 
 from ocs.ocs_agent import OpSession
 
@@ -12,25 +13,27 @@ from unittest import mock
 import txaio
 txaio.use_twisted()
 
-def create_agent(base_dir, file_duration=10):
+def create_agent(base_dir, file_duration=10, frame_len=2,
+                 nchans=1024):
     """Test fixture to setup a mocked OCSAgent."""
     mock_agent = mock.MagicMock()
     log = txaio.make_logger()
     txaio.start_logging(level='debug')
     mock_agent.log = log
     log.info('Initialized mock OCSAgent')
-    args = Namespace(
-        stream_id='test_em', base_dir=base_dir,
-        stream_on_start=False, file_duration=file_duration
-    )
-
+    parser = make_parser()
+    args = parser.parse_args(args=[
+        '--stream-id', 'test_em', '--base-dir', base_dir,
+        '--file-duration', str(file_duration), '--frame-len', str(frame_len)
+    ])
     agent = SmurfFileEmulator(mock_agent, args)
     return agent
 
 def test_tune_up(tmp_path):
     emulator = create_agent(str(tmp_path))
     session = mock.MagicMock()
-    emulator.tune_dets(session)
+    session.data = {}
+    emulator.tune_dets(session, {'test_mode': True})
 
 def test_take_iv(tmp_path):
     emulator = create_agent(str(tmp_path))
@@ -47,7 +50,9 @@ def test_bias_dets(tmp_path):
     session = mock.MagicMock()
     emulator.bias_dets(session)
 
+
 def test_stream(tmp_path):
-    emulator = create_agent(str(tmp_path), file_duration=0.2)
+    emulator = create_agent(str(tmp_path), file_duration=0.2, frame_len=0.05)
     session = mock.MagicMock()
+    session.data ={}
     emulator.stream(session, params={'duration': 1})
