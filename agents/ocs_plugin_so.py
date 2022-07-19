b8eaab49fea37a31aa6cfc603856da499c053feb
ykyohei <38639108+ykyohei@users.noreply.github.com>
2022-07-20 07:21:49 +0900

    fix ocs_plugin_so.py

--- agents/ocs_plugin_so.py
+++ agents/ocs_plugin_so.py
@@ -19,7 +19,7 @@ for n, f in [
         ('LATRtXYStageAgent', 'xy_stage/xy_latrt_agent.py'),
         ('ACUAgent', 'acu/acu_agent.py'),
         ('FTSAerotechAgent', 'fts_aerotech_stage/fts_aerotech_agent.py'),
-        ('VantagePro2Agent', 'vantagePro2_agent/vantage_pro2_agent.py')
-        ('HWPPicoscopeAgent', 'hwp_picoscope/pico_agent.py')
+        ('VantagePro2Agent', 'vantagePro2_agent/vantage_pro2_agent.py'),
+        ('HWPPicoscopeAgent', 'hwp_picoscope/pico_agent.py'),
 ]:
     ocs.site_config.register_agent_class(n, os.path.join(root, f))
