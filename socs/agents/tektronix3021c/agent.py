b7f4ce51e5993940435d05e8d20120798b76294e
Brian Koopman <brian.koopman@yale.edu>
2022-11-04 20:07:02 -0400

    Standardize TektronixAWGAgent docs

--- socs/agents/tektronix3021c/agent.py
+++ socs/agents/tektronix3021c/agent.py
@@ -2,26 +2,22 @@
 mrandall@ucsd.edu"""
 
 import argparse
-import os
 import socket
 import time
 
-from socs.agents.tektronix3021c.drivers import TektronixInterface
+from ocs import ocs_agent, site_config
+from ocs.ocs_twisted import TimeoutLock
 
-on_rtd = os.environ.get('READTHEDOCS') == 'True'
-if not on_rtd:
-    from ocs import ocs_agent, site_config
-    from ocs.ocs_twisted import TimeoutLock
+from socs.agents.tektronix3021c.drivers import TektronixInterface
 
 
 class TektronixAWGAgent:
-    """Tektronix3021c Agent.
+    """Tektronix3021c Function Generator Agent.
 
     Args:
-        ip_address (string): the IP address of the gpib to ethernet
+        ip_address (string): The IP address of the gpib to ethernet
             controller connected to the function generator.
-
-        gpib_slot (int): the gpib address currently set
+        gpib_slot (int): The gpib address currently set
             on the function generator.
 
     """
@@ -46,8 +42,13 @@ class TektronixAWGAgent:
                                  record=True,
                                  agg_params=agg_params)
 
-    def init_awg(self, session, params=None):
-        """ Task to connect to Tektronix AWG """
+    @ocs_agent.param('_')
+    def init(self, session, params=None):
+        """init()
+
+        **Task** - Initialize connection to Tektronix AWG.
+
+        """
 
         with self.lock.acquire_timeout(0) as acquired:
             if not acquired:
@@ -66,108 +67,73 @@ class TektronixAWGAgent:
 
         return True, 'Initialized AWG.'
 
+    @ocs_agent.param('frequency', type=float, check=lambda x: 0 <= x <= 25e6)
     def set_frequency(self, session, params=None):
-        """
-        Sets frequency of function generator:
+        """set_frequency(frequency)
 
-        Args:
-            frequency (float): Frequency to set in Hz.
-            Must be between 0 and 25,000,000.
+        **Task** - Set frequency of the function generator.
+
+        Parameters:
+            frequency (float): Frequency to set in Hz. Must be between 0 and
+                25,000,000.
         """
 
         with self.lock.acquire_timeout(1) as acquired:
             if acquired:
-                freq = params.get("frequency")
+                freq = params['frequency']
 
-                try:
-                    float(freq)
+                self.awg.set_freq(freq)
 
-                except ValueError as e:
-                    return False, """Frequency must
-                                    be a float or int -> {}""".format(e)
-
-                except TypeError as e:
-                    return False, """Frequency must
-                                    not be of NoneType -> {}""".format(e)
-
-                if 0 < freq < 25E6:
-                    self.awg.set_freq(freq)
-
-                    data = {'timestamp': time.time(),
-                            'block_name': "AWG_frequency_cmd",
-                            'data': {'AWG_frequency_cmd': freq}
-                            }
-                    self.agent.publish_to_feed('awg', data)
-
-                else:
-                    return False, """Invalid input:
-                        Frequency must be between 0 and 25,000,000 Hz"""
-
+                data = {'timestamp': time.time(),
+                        'block_name': "AWG_frequency_cmd",
+                        'data': {'AWG_frequency_cmd': freq}
+                        }
+                self.agent.publish_to_feed('awg', data)
             else:
                 return False, "Could not acquire lock"
 
-        return True, 'Set frequency {} Hz'.format(params)
+        return True, 'Set frequency {} Hz'.format(freq)
 
+    @ocs_agent.param('amplitude', type=float, check=lambda x: 0 <= x <= 10)
     def set_amplitude(self, session, params=None):
-        """
-        Sets current of power supply:
+        """set_amplitude(amplitude)
 
-        Args:
-            amplitude (float): Peak to Peak voltage to set.
-            Must be between 0 and 10.
+        **Task** - Set peak to peak voltage of the function generator.
+
+        Parameters:
+            amplitude (float): Peak to Peak voltage to set. Must be between 0
+                and 10.
+
         """
         with self.lock.acquire_timeout(1) as acquired:
             if acquired:
-                amp = params.get('amplitude')
-                try:
-                    float(amp)
+                amp = params['amplitude']
 
-                except ValueError as e:
-                    return False, """Amplitude must be
-                                    a float or int -> {}""".format(e)
+                self.awg.set_amp(amp)
 
-                except TypeError as e:
-                    return False, """Amplitude must not be
-                                    of NoneType -> {}""".format(e)
-
-                if 0 < amp < 10:
-                    self.awg.set_amp(amp)
-
-                    data = {'timestamp': time.time(),
-                            'block_name': "AWG_amplitude_cmd",
-                            'data': {'AWG_amplitude_cmd': amp}
-                            }
-                    self.agent.publish_to_feed('awg', data)
-
-                else:
-                    return False, """Amplitude must be
-                                    between 0 and 10 Volts peak to peak"""
-
+                data = {'timestamp': time.time(),
+                        'block_name': "AWG_amplitude_cmd",
+                        'data': {'AWG_amplitude_cmd': amp}
+                        }
+                self.agent.publish_to_feed('awg', data)
             else:
                 return False, "Could not acquire lock"
 
         return True, 'Set amplitude to {} Vpp'.format(params)
 
+    @ocs_agent.param('state', type=bool)
     def set_output(self, session, params=None):
-        """
-        Task to turn channel on or off.
+        """set_output(state)
 
-        Args:
+        **Task** - Turn function generator output on or off.
+
+        Parameters:
             state (bool): True for on, False for off.
+
         """
         with self.lock.acquire_timeout(1) as acquired:
             if acquired:
                 state = params.get("state")
-
-                try:
-                    bool(state)
-
-                except ValueError as e:
-                    return False, "State must be a boolean -> {}".format(e)
-
-                except TypeError as e:
-                    return False, """State must not
-                                    be of NoneType -> {}""".format(e)
 
                 self.awg.set_output(state)
 
@@ -189,9 +155,9 @@ def make_parser(parser=None):
 
     pgroup = parser.add_argument_group('Agent Options')
     pgroup.add_argument('--ip-address', type=str,
-                        help="IP address of tektronix device")
+                        help="IP address of Tektronix device.")
     pgroup.add_argument('--gpib-slot', type=int,
-                        help="GPIB slot of tektronix device")
+                        help="GPIB slot of Tektronix device.")
     return parser
 
 
@@ -205,7 +171,7 @@ def main(args=None):
 
     p = TektronixAWGAgent(agent, args.ip_address, args.gpib_slot)
 
-    agent.register_task('init', p.init_awg, startup=True)
+    agent.register_task('init', p.init, startup=True)
     agent.register_task('set_frequency', p.set_frequency)
     agent.register_task('set_amplitude', p.set_amplitude)
     agent.register_task('set_output', p.set_output)
