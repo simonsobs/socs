702af44481d1cdbca34e3c217b73e51ff59fdb8f
David Nguyen <david.nguyen@yale.edu>
2023-01-09 13:26:35 -0500

    UPS agent & integration test (#362)
    * initial UPS Agent
    
    * ups integration test
    
    * added doc
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * requirements
    
    * add mib to flake8
    
    * removed unused imports
    
    * mode args consistency
    
    * removed auto-start
    
    * ups: Move ups agent into package
    
    * added UPS models and removed typos
    
    * added more detailed docstrings
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Brian Koopman <brian.koopman@yale.edu>

---
+++ tests/integration/test_ups_agent_integration.py
@@ -0,0 +1,61 @@
+import os
+import signal
+import subprocess
+from multiprocessing import Process
+from unittest.mock import patch
+
+import ocs
+import pytest
+from integration.util import create_crossbar_fixture
+from ocs.base import OpCode
+from ocs.testing import create_agent_runner_fixture, create_client_fixture
+from snmpsim.commands import responder
+
+pytest_plugins = "docker_compose"
+
+wait_for_crossbar = create_crossbar_fixture()
+run_agent = create_agent_runner_fixture(
+    "../socs/agents/ups/agent.py",
+    "UPSAgent",
+    args=["--log-dir", "./logs/"],
+)
+client = create_client_fixture("ups")
+
+subprocess.run(
+    "mkdir -p ~/.pysnmp/mibs && cp -r ../socs/mibs/. ~/.pysnmp/mibs",
+    shell=True,
+)
+
+address = "127.0.0.1"
+port = 1024
+
+
+def check_resp_success(resp):
+    print(resp)
+    assert resp.status == ocs.OK
+    print(resp.session)
+    assert resp.session["op_code"] == OpCode.SUCCEEDED.value
+
+
+@pytest.fixture
+def start_responder():
+    with patch(
+        "sys.argv",
+        [
+            "test_ups_agent_integration.py",
+            "--data-dir=./integration/ups_snmp_data",
+            f"--agent-udpv4-endpoint={address}:{port}",
+            # f"--variation-modules-dir={os.path.expanduser('~/.local/share/snmpsim/variation')}",
+        ],
+    ):
+        p = Process(target=responder.main)
+        p.start()
+        yield
+        os.kill(p.pid, signal.SIGINT)
+
+
+@pytest.mark.integtest
+def test_ups_acq(wait_for_crossbar, start_responder, run_agent, client):
+    resp = client.acq.start(test_mode=True)
+    resp = client.acq.wait()
+    check_resp_success(resp)
