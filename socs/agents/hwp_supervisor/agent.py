dbe60ecd761eea143abcafc02b030736627d62e0
Jack Lashner <jlashner@gmail.com>
2023-04-14 18:10:06 -0400

    Updates to HWP supervisor
    from testing with ocs-web mock agents.

--- socs/agents/hwp_supervisor/agent.py
+++ socs/agents/hwp_supervisor/agent.py
@@ -1,6 +1,5 @@
 import argparse
 import time
-from collections import defaultdict
 
 import txaio
 from ocs import client_http, ocs_agent, site_config
@@ -96,20 +95,105 @@ class HWPSupervisor:
         self.hwp_encoder_id = args.hwp_encoder_id
         self.hwp_rotation_id = args.hwp_rotation_id
         self.ups_id = args.ups_id
+        self._ups_oid = None
+        self.ups_minutes_remaining_thresh = args.ups_minutes_remaining_thresh
 
-    def parse_hwp_temp(self, op_data):
-        if op_data['status'] != 'ok':
-            return None, 'no_data'
+    def _get_rotator_action(self, state):
+        # First check if either ups or temp are beyond a threshold
+        hwp_temp = state['hwp_temp']
+        if hwp_temp is not None and self.hwp_temp_thresh is not None:
+            if hwp_temp > self.hwp_temp_thresh:
+                return 'stop'
+        
+        minutes_remaining = state['ups_estimated_minutes_remaining']
+        if minutes_remaining is not None:
+            if minutes_remaining < self.ups_minutes_remaining_thresh:
+                return 'stop'
+        
+        # If either hwp_temp or ups state is None, return no_data
+        for val in [hwp_temp, minutes_remaining]:
+            if val is None:
+                return 'no_data'
+        
+        return 'ok'
+    
+    def _get_gripper_action(self, state):
+        """
+        Gets the gripper action based on the current state of the system.
+        This will only report 'stop' if the rot_action is 'stop' and the hwp
+        freq is exactly 0. The gripper agent should not grip the hwp in a
+        no-data event.
+        """
+        rot_action = self._get_rotator_action(state)
+        if rot_action == 'ok':
+            return 'ok'
+        if rot_action == 'no_data':
+            return 'no_data'
+        
+        hwp_freq = state['hwp_freq']
+        if hwp_freq is None:
+            return 'no_data'  
+        elif hwp_freq > 0:
+            return 'ok'
+        else:  # Only grip if the hwp_freq is exactly 0
+            return 'stop'
 
-        fields = op_data['data']['fields']
+    def _update_state_temp(self, temp_op, state):
+        """
+        Updates state dict with temperature data.
+        """
+        state.update({
+            'hwp_temp': None,
+            'hwp_temp_status': 'no_data',
+            'hwp_temp_thresh': self.hwp_temp_thresh,
+        })
+
+        if temp_op['status'] != 'ok':
+            return state
+
+        fields = temp_op['data']['fields']
         if self.hwp_temp_field not in fields:
-            return None, 'no_data'
+            return state
 
         hwp_temp = fields[self.hwp_temp_field]['T']
-        if hwp_temp > self.hwp_temp_thresh:
-            return hwp_temp, 'over'
-        else:
-            return hwp_temp, 'ok'
+        state['hwp_temp'] = hwp_temp
+        state['hwp_temp_status'] = 'ok'
+
+        if self.hwp_temp_thresh is not None:
+            if hwp_temp > self.hwp_temp_thresh:
+                state['hwp_temp_status'] = 'over'
+
+        return state
+    
+    def _update_state_ups(self, ups_op, state):
+        """
+        Updates state dict with UPS data.
+        """
+        ups_keymap = {
+            'ups_output_source': ('upsOutputSource', 'description'),
+            'ups_estimated_minutes_remaining': ('upsEstimatedMinutesRemaining', 'status'),
+            'ups_battery_voltags': ('upsBatteryVoltage', 'status'),
+            'ups_battery_current': ('upsBatteryCurrent', 'status'),
+        }
+
+        state['ups_minutes_remaining_thresh'] = self.ups_minutes_remaining_thresh
+
+        if ups_op['status'] != 'ok':
+            for k in ups_keymap:
+                state[k] = None
+            return
+        
+        # get oid
+        data = ups_op['data']
+        for k in data:
+            if k.startswith('upsOutputSource'):
+                ups_oid = k.split('_')[1]
+                break
+        
+        for k, field in ups_keymap.items():
+            state[k] = data[f'{field[0]}_{ups_oid}'][field[1]]
+        
+        return state
 
     @ocs_agent.param('test_mode', type=bool, default=False)
     def monitor(self, session, params):
@@ -176,37 +260,32 @@ class HWPSupervisor:
             enc_op = get_op_data(self.hwp_encoder_id, 'acq', **kw)
             rot_op = get_op_data(self.hwp_rotation_id, 'iv_acq', **kw)
             ups_op = get_op_data(self.ups_id, 'acq', **kw)
+
             session.data['monitored_sessions'] = {
-                'temperature': temp_op, 'encoder': enc_op, 'rotation': rot_op,
+                'temperature': temp_op, 
+                'encoder': enc_op, 
+                'rotation': rot_op,
                 'ups': ups_op
             }
 
-            # Parse data for relevant info
-            hwp_temp, hwp_temp_status = self.parse_hwp_temp(temp_op)
+            # gather state info 
+            state = {}
+            self._update_state_temp(temp_op, state)
+            self._update_state_ups(ups_op, state)
 
-            hwp_freq = None
             if enc_op['status'] == 'ok':
-                hwp_freq = enc_op['data']['approx_hwp_freq']
+                state['hwp_freq'] = enc_op['data']['approx_hwp_freq']
+            else:
+                state['hwp_freq'] = None
 
-            # TODO: Add UPS state
-
-            session.data['state'] = {
-                'hwp_temp': hwp_temp, 'hwp_temp_status': hwp_temp_status,
-                'hwp_freq': hwp_freq,
-            }
-
+            session.data['state'] = state
+            
             # Get actions for each hwp subsystem
-            rot_action = 'ok'
-            if hwp_temp_status == 'over':
-                rot_action = 'stop'
-            elif hwp_temp_status == 'no_data':
-                rot_action = 'no_data'
-
-            # TODO: Add gripper and encoder action
-
             session.data['actions'] = {
-                'rotation': rot_action
+                'rotation': self._get_rotator_action(state),
+                'gripper': self._get_gripper_action(state),
             }
+
 
             if test_mode:
                 break
@@ -239,6 +318,9 @@ def make_parser(parser=None):
     pgroup.add_argument('--hwp-rotation-id',
                         help="Instance ID for rotation agent")
     pgroup.add_argument('--ups-id', help="Instance ID for UPS agent")
+    pgroup.add_argument('--ups-minutes-remaining-thresh', type=float, default=30.,
+                        help="Threshold for UPS minutes remaining before a "
+                             "shutdown is triggered")
 
     return parser
 
