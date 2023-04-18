ef39c1517d6290b667c0d482fd0e6c07e986fc44
simonsobs <simonsobs@example.com>
2023-04-18 20:27:35 +0200

    ACU: fix timecode function end-of-year bugs

--- socs/agents/acu/agent.py
+++ socs/agents/acu/agent.py
@@ -21,18 +21,33 @@ import socs.agents.acu.drivers as sh
 #: The number of free ProgramTrack positions, when stack is empty.
 FULL_STACK = 10000
 
+#: The number of seconds in a day.
+DAY = 86400
 
-def timecode(acutime):
-    """
-    Takes the time code produced by the ACU status stream and returns
-    a ctime.
+
+def timecode(acutime, now=None):
+    """Takes the time code produced by the ACU status stream and returns
+    a unix timestamp.
 
     Parameters:
         acutime (float): The time recorded by the ACU status stream,
-                         corresponding to the fractional day of the year
+            corresponding to the fractional day of the year.
+        now (float): The time, as unix timestamp, to assume it is now.
+            This is for testing, it defaults to time.time().
+
     """
-    sec_of_day = (acutime - 1) * 60 * 60 * 24
-    year = datetime.datetime.now().year
+    sec_of_day = (acutime - 1) * DAY
+    if now is None:
+        now = time.time()  # testing
+
+    # This guard protects us at end of year, when time.time() and
+    # acutime might correspond to different years.
+    if acutime > 180:
+        context = datetime.datetime.utcfromtimestamp(now - 30 * DAY)
+    else:
+        context = datetime.datetime.utcfromtimestamp(now + 30 * DAY)
+
+    year = context.year
     gyear = calendar.timegm(time.strptime(str(year), '%Y'))
     comptime = gyear + sec_of_day
     return comptime
