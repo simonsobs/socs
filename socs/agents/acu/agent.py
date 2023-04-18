5f724c743054f6c341445d023e7581f3e5c296a5
simonsobs <simonsobs@example.com>
2023-04-18 20:33:24 +0200

    ACU: move timecode function into drivers.py

--- socs/agents/acu/agent.py
+++ socs/agents/acu/agent.py
@@ -21,37 +21,6 @@ import socs.agents.acu.drivers as sh
 #: The number of free ProgramTrack positions, when stack is empty.
 FULL_STACK = 10000
 
-#: The number of seconds in a day.
-DAY = 86400
-
-
-def timecode(acutime, now=None):
-    """Takes the time code produced by the ACU status stream and returns
-    a unix timestamp.
-
-    Parameters:
-        acutime (float): The time recorded by the ACU status stream,
-            corresponding to the fractional day of the year.
-        now (float): The time, as unix timestamp, to assume it is now.
-            This is for testing, it defaults to time.time().
-
-    """
-    sec_of_day = (acutime - 1) * DAY
-    if now is None:
-        now = time.time()  # testing
-
-    # This guard protects us at end of year, when time.time() and
-    # acutime might correspond to different years.
-    if acutime > 180:
-        context = datetime.datetime.utcfromtimestamp(now - 30 * DAY)
-    else:
-        context = datetime.datetime.utcfromtimestamp(now + 30 * DAY)
-
-    year = context.year
-    gyear = calendar.timegm(time.strptime(str(year), '%Y'))
-    comptime = gyear + sec_of_day
-    return comptime
-
 
 class ACUAgent:
     """
@@ -407,7 +376,7 @@ class ACUAgent:
                                 else:
                                     self.data['status'][category][self.monitor_fields[category][key]] = str(value)
             self.data['status']['summary']['ctime'] =\
-                timecode(self.data['status']['summary']['Time'])
+                sh.timecode(self.data['status']['summary']['Time'])
             if self.data['status']['platform_status']['Remote_mode'] == 0:
                 if was_remote:
                     was_remote = False
--- socs/agents/acu/drivers.py
+++ socs/agents/acu/drivers.py
@@ -1,7 +1,12 @@
+import calendar
+import datetime
 import math
 import time
 
 import numpy as np
+
+#: The number of seconds in a day.
+DAY = 86400
 
 
 def constant_velocity_scanpoints(azpts, el, azvel, acc, ntimes):
@@ -167,6 +172,34 @@ def ptstack_format(conctimes, concaz, concel, concva, concve, az_flags,
                  for n in range(len(fmt_times))]
 
     return all_lines
+
+
+def timecode(acutime, now=None):
+    """Takes the time code produced by the ACU status stream and returns
+    a unix timestamp.
+
+    Parameters:
+        acutime (float): The time recorded by the ACU status stream,
+            corresponding to the fractional day of the year.
+        now (float): The time, as unix timestamp, to assume it is now.
+            This is for testing, it defaults to time.time().
+
+    """
+    sec_of_day = (acutime - 1) * DAY
+    if now is None:
+        now = time.time()  # testing
+
+    # This guard protects us at end of year, when time.time() and
+    # acutime might correspond to different years.
+    if acutime > 180:
+        context = datetime.datetime.utcfromtimestamp(now - 30 * DAY)
+    else:
+        context = datetime.datetime.utcfromtimestamp(now + 30 * DAY)
+
+    year = context.year
+    gyear = calendar.timegm(time.strptime(str(year), '%Y'))
+    comptime = gyear + sec_of_day
+    return comptime
 
 
 def generate_constant_velocity_scan(az_endpoint1, az_endpoint2, az_speed,
