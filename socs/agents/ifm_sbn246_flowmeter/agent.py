60b6338ee222166b15bca6ad9cad6d6c75f1a453
sanahabhimani <sanahbhimani@gmail.com>
2023-04-24 22:14:22 -0400

    clean up code, docstrings, and invoke flake8

--- socs/agents/ifm_sbn246_flowmeter/agent.py
+++ socs/agents/ifm_sbn246_flowmeter/agent.py
@@ -1,15 +1,15 @@
 import txaio
-import argparse
+import time
 from pyModbusTCP.client import ModbusClient
 
 from os import environ
-from ocs import ocs_agent, site_config
-from ocs.ocs_twisted import TimeoutLock
 
-# TODO: need driver code that knows which register address info to grab 
-# based on port you attach things to
+on_rtd = environ.get('READTHEDOCS') == 'True'
+if not on_rtd:
+    from ocs import ocs_agent, site_config
+    from ocs.ocs_twisted import TimeoutLock, Pacemaker
 
-# dictionary for defining register address given the port you plug into
+# dictionary for defining register address given the daq port you plug into
 daq_ports = {1: 1002,
              2: 2002,
              3: 3002,
@@ -22,29 +22,30 @@ class FlowmeterAgent:
     ----------
     agent : OCS Agent
         OCSAgent object which forms this Agent
-    ip: str
-        IP address of IFM DAQ IO devicer
+    ip_address: str
+        IP address of IFM DAQ IO device
     port : int
         Port for the ip address
-    daq_port: string
-        port on daq IO device that plugs into the IFM devices
+    daq_port: int
+        Port on daq IO device that connects to flowmeter
     unit_id : int
-        # TODO: idk yet
+        Unit identifier for Modbus bridge that allows for communication
+        across multiple ports on the daq device while using the same
+        ip address. Default unit identifier is 502
     auto_open : bool
-        # TODO
+        State for automatically keeping TCP connection open
     auto_close : bool
-        # TODO
+        State for automatically closing TCP connection
     """
-    def __init__(self, agent, ip_address, daq_port, port=502, unit_id=1, auto_open=True, auto_close=False):
+    def __init__(self, agent, ip_address, daq_port, port=502, unit_id=1, auto_open=True, auto_close=True):
         self.agent = agent
         self.log = agent.log
         self.lock = TimeoutLock()
 
-         # TODO: fix the order
-
         self.ip_address = ip_address
         self.port = port
         self.daq_port = daq_port
+        self.unit_id = unit_id
         self.auto_open = auto_open
         self.auto_close = auto_close
 
@@ -63,33 +64,34 @@ class FlowmeterAgent:
     @ocs_agent.param('test_mode', default=False, type=bool)
     def acq(self, session, params=None):
         """acq()
-        **Process** - Fetch values from the Elnet Power Meter
+        **Process** - Fetch values from the flowmeter using the DAQ device
         Parameters
         ----------
         test_mode : bool, option
             Run the Process loop only once. Meant only for testing.
             Default is False.
         """
+        pm = Pacemaker(1, quantize=True)
         self.take_data = True
         while self.take_data:
+            pm.sleep()
             m = ModbusClient(host=self.ip_address, port=self.port, unit_id=self.unit_id, auto_open=self.auto_open, auto_close=self.auto_close)
-           
-            # and then you need to split it into a float value; i think this should all be done as driver code above the class
-            register = int(daq_ports[self.daq_port])
-            print('register1', register)
-            register_temp = register + 1
-            register_temp = int(register_temp)
-            print('register_temp', register_temp)
 
-            
-            flow = m.read_holding_registers(register, 1)
-            temp = m.read_holding_registers(register_temp, 1)
+            register = int(daq_ports[self.daq_port])
+            sensor_data = m.read_holding_registers(register, 2)
+            flow = sensor_data[0]  # gallons/min
+            temp = sensor_data[1]  # Celsius
+
+            # convert flow and temp into floats
+            flow = flow / 10
+            temp = temp / 10
 
             data = {'block_name': 'flowmeter',
                     'timestamp': time.time(),
-                    'data' :{'flow': flow,
+                    'data': {'flow': flow,
                              'temp': temp}
                     }
+            print('data', data)
 
             self.agent.publish_to_feed('flowmeter', data)
 
@@ -117,9 +119,10 @@ def add_agent_args(parser_in=None):
     pgroup.add_argument("--port", type=int, default=5021, help="PyModbusTCP port for querying information from the DAQ Modbus TCP port")
     pgroup.add_argument("--unit-id", type=int, default=1, help="Unit ID for pymodbus TCP protocol")
     pgroup.add_argument("--auto-open", type=bool, default=True, help="state for automatically keeping TCP connection open")
-    pgroup.add_argument("--auto-close", type=bool, default=False, help="state for automatically closing TCP connection")
+    pgroup.add_argument("--auto-close", type=bool, default=True, help="state for automatically closing TCP connection")
 
     return parser_in
+
 
 def main(args=None):
     # For logging
@@ -137,6 +140,7 @@ def main(args=None):
     agent.register_process('acq', f.acq, f._stop_acq, startup=True)
 
     runner.run(agent, auto_reconnect=True)
+
 
 if __name__ == "__main__":
     main()
