8eeef95f353c62b5b644d3192a19d76dd6d1a676
sanahabhimani <sanahbhimani@gmail.com>
2023-04-26 11:24:30 -0400

    add flowmeter agent that queries values using http requests

--- socs/agents/ifm_sbn246_flowmeter/agent.py
+++ socs/agents/ifm_sbn246_flowmeter/agent.py
@@ -1,20 +1,37 @@
 import time
 from os import environ
+import requests
 
 import txaio
 from ocs import ocs_agent, site_config
 from ocs.ocs_twisted import Pacemaker, TimeoutLock
 
-on_rtd = environ.get('READTHEDOCS') == 'True'
-if not on_rtd:
-    from pyModbusTCP.client import ModbusClient
+# TODO: fix below, not sure we need the on_rtd since we already have the requests package
+#on_rtd = environ.get('READTHEDOCS') == 'True'
+#if not on_rtd:
+#    from pyModbusTCP.client import ModbusClient
 
 
-# dictionary for defining register address given the daq port you plug into
-daq_ports = {1: 1002,
-             2: 2002,
-             3: 3002,
-             4: 4002}
+def extract(value):
+    """
+    Extract flow and temp from raw hexidecimal value from SBN246 Process Data.
+
+    Args:
+        value (str): Hexidecimal value from SBN246 Process Data.
+
+    Returns:
+        float, int: The flow in units of gallons per minute (gpm) and
+        temperature in units of F.
+
+    """
+    binary = bin(int(value, 16))[2:].zfill(32)
+    _b_flow = binary[0:16]
+    _b_temp = binary[17:30]
+
+    flow = int(_b_flow, 2) / 10
+    temp = int(_b_temp, 2)
+
+    return flow, temp
 
 
 class FlowmeterAgent:
@@ -25,33 +42,20 @@ class FlowmeterAgent:
     ----------
     agent : OCS Agent
         OCSAgent object which forms this Agent
-    ip_address: str
-        IP address of IFM DAQ IO device
-    port : int
-        Port for the ip address. Default is 502 for the DAQ device
+    url: str
+        HTTP url of the DAQ IO-Link device to make requests from
+        Ex: 'http://10.10.10.159'
     daq_port: int
-        Port on daq IO device that connects to flowmeter
-    unit_id : int
-        Unit identifier for Modbus bridge that allows for communication
-        across multiple ports on the daq device while using the same
-        ip address. Default unit identifier is 1
-    auto_open : bool
-        State for automatically keeping TCP connection open
-    auto_close : bool
-        State for automatically closing TCP connection
+        Port on daq IO device that connects to flowmeter. Choices are 1-4
     """
 
-    def __init__(self, agent, ip_address, daq_port, port=502, unit_id=1, auto_open=True, auto_close=True):
+    def __init__(self, agent, url, daq_port):
         self.agent = agent
         self.log = agent.log
         self.lock = TimeoutLock()
 
-        self.ip_address = ip_address
-        self.port = port
+        self.url = url
         self.daq_port = daq_port
-        self.unit_id = unit_id
-        self.auto_open = auto_open
-        self.auto_close = auto_close
 
         self.take_data = False
 
@@ -82,22 +86,22 @@ class FlowmeterAgent:
         self.take_data = True
         while self.take_data:
             pm.sleep()
-            m = ModbusClient(host=self.ip_address, port=self.port, unit_id=self.unit_id, auto_open=self.auto_open, auto_close=self.auto_close)
 
-            register = int(daq_ports[self.daq_port])
-            sensor_data = m.read_holding_registers(register, 2)
-            flow = sensor_data[0]  # gallons/min
-            temp = sensor_data[1]  # Celsius
+            dp = int(self.daq_port)
+            adr = "/iolinkmaster/port[{}]/iolinkdevice/pdin/getdata".format(dp)
 
-            # convert flow and temp into floats
-            flow = flow / 10
-            temp = temp / 10
+            r = requests.post(self.url, json={"code": "request", "cid": -1, "adr": adr})
+            value = r.json()['data']['value']
+
+            flow, temp = extract(value)
 
             data = {'block_name': 'flowmeter',
                     'timestamp': time.time(),
                     'data': {'flow': flow,
                              'temp': temp}
                     }
+
+            print(data)
 
             self.agent.publish_to_feed('flowmeter', data)
 
@@ -120,12 +124,8 @@ def add_agent_args(parser_in=None):
         from argparse import ArgumentParser as A
         parser_in = A()
     pgroup = parser_in.add_argument_group('Agent Options')
-    pgroup.add_argument("--ip-address", type=str, default='localhost', help="IP address of IFM DAQ IO device connected to IFM flowmeter")
-    pgroup.add_argument("--daq-port", type=int, default=1, help="Port on DAQ IO-Link device that IFM is connected to")
-    pgroup.add_argument("--port", type=int, default=502, help="PyModbusTCP port for querying from DAQ IO Link device")
-    pgroup.add_argument("--unit-id", type=int, default=1, help="Unit ID for pymodbus TCP protocol")
-    pgroup.add_argument("--auto-open", type=bool, default=True, help="State for automatically keeping TCP connection open")
-    pgroup.add_argument("--auto-close", type=bool, default=True, help="State for automatically closing TCP connection")
+    pgroup.add_argument("--url", type=str, help="HTTP address of DAQ IO-Link device. Ex: 'http://10.10.10.159'")
+    pgroup.add_argument("--daq-port", type=int, help="Port on DAQ IO-Link device that IFM is connected to")
 
     return parser_in
 
@@ -141,7 +141,7 @@ def main(args=None):
     args = site_config.parse_args(agent_class='FlowmeterAgent', parser=parser, args=args)
 
     agent, runner = ocs_agent.init_site_agent(args)
-    f = FlowmeterAgent(agent, args.ip_address, args.daq_port, args.port, args.unit_id, args.auto_open, args.auto_close)
+    f = FlowmeterAgent(agent, args.url, args.daq_port)
 
     agent.register_process('acq', f.acq, f._stop_acq, startup=True)
 
