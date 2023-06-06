1ceebe1db73bdc647801dfba7d3fe6fbce23ae28
David Nguyen <david.nguyen@yale.edu>
2023-06-06 15:54:14 -0400

    Updates to ibootbar Agent (#446)
    * connection fix attempt
    
    * added ip address to session.data and default acq mode
    
    * added task to lock/unlock an outlet
    
    * added lock_outlet task to integration tests
    
    * lock outlets on startup with args
    
    * updated docstrings
    
    * removed except block and updated docstrings with outlet numbering
    
    * addressed comments
    
    * added locked state to session.data, updated docstrings
    
    * sample period wording
    
    * lock outlet integration tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- socs/agents/ibootbar/agent.py
+++ socs/agents/ibootbar/agent.py
@@ -100,7 +100,7 @@ def _build_message(get_result, names, time):
     return message
 
 
-def update_cache(get_result, names, timestamp):
+def update_cache(get_result, names, outlet_locked, timestamp):
     """Update the OID Value Cache.
 
     The OID Value Cache is used to store each unique OID and will be passed to
@@ -124,29 +124,31 @@ def update_cache(get_result, names, timestamp):
         Result from a pysnmp GET command.
     names : list
         List of strings for outlet names
+    outlet_locked : list of bool
+        List of bool for outlets (1-8) that are locked
     timestamp : float
         Timestamp for when the SNMP GET was issued.
     """
     oid_cache = {}
-    try:
-        for item in get_result:
-            field_name, oid_value, oid_description = _extract_oid_field_and_value(item)
-            if oid_value is None:
-                continue
-
-            # Update OID Cache for session.data
-            oid_cache[field_name] = {"status": oid_value}
-            oid_cache[field_name]["name"] = names[int(field_name[-1])]
-            oid_cache[field_name]["description"] = oid_description
-            oid_cache['ibootbar_connection'] = {'last_attempt': time.time(),
-                                                'connected': True}
-            oid_cache['timestamp'] = timestamp
-    # This is a TypeError due to nothing coming back from the yield,
-    # so get_result is None here and can't be iterated.
-    except TypeError:
+    # Return disconnected if SNMP response is empty
+    if get_result is None:
         oid_cache['ibootbar_connection'] = {'last_attempt': time.time(),
                                             'connected': False}
-        raise ConnectionError('No SNMP response. Check your connection.')
+        return oid_cache
+
+    for item in get_result:
+        field_name, oid_value, oid_description = _extract_oid_field_and_value(item)
+        if oid_value is None:
+            continue
+
+        # Update OID Cache for session.data
+        oid_cache[field_name] = {"status": oid_value}
+        oid_cache[field_name]["name"] = names[int(field_name[-1])]
+        oid_cache[field_name]["locked"] = outlet_locked[int(field_name[-1])]
+        oid_cache[field_name]["description"] = oid_description
+        oid_cache['ibootbar_connection'] = {'last_attempt': time.time(),
+                                            'connected': True}
+        oid_cache['timestamp'] = timestamp
 
     return oid_cache
 
@@ -164,6 +166,8 @@ class ibootbarAgent:
         SNMP port to issue GETs to, default to 161.
     version : int
         SNMP version for communication (1, 2, or 3), defaults to 2.
+    lock_outlet : list of ints
+        List of outlets to lock on agent startup. Outlets are numbered 1-8.
 
     Attributes
     ----------
@@ -176,17 +180,28 @@ class ibootbarAgent:
         txaio logger object, created by the OCSAgent
     """
 
-    def __init__(self, agent, address, port=161, version=2):
+    def __init__(self, agent, address, port=161, version=2, lock_outlet=None):
         self.agent = agent
         self.is_streaming = False
         self.log = self.agent.log
         self.lock = TimeoutLock()
 
+        # Initialize with locked outlets if given in args
+        self.outlet_locked = [False for i in range(8)]
+        if lock_outlet is not None:
+            for i in range(8):
+                if i + 1 in lock_outlet:
+                    self.outlet_locked[i] = True
+                else:
+                    self.outlet_locked[i] = False
+
         self.log.info(f'Using SNMP version {version}.')
         self.version = version
+        self.address = address
         self.snmp = SNMPTwister(address, port)
 
         self.lastGet = 0
+        self.sample_period = 60
 
         agg_params = {
             'frame_length': 10 * 60  # [sec]
@@ -221,19 +236,22 @@ class ibootbarAgent:
              'ibootbar_connection':
                 {'last_attempt': 1656085022.680916,
                  'connected': True},
-             'timestamp': 1656085022.680916}
+             'timestamp': 1656085022.680916,
+             'address': '10.10.10.50'}
         """
         # Set initial default outlet names
+        # Note outlets are numbered 1-8 physically on ibootbars, OIDs are numbered 0-7
         names = ['Outlet-1', 'Outlet-2', 'Outlet-3', 'Outlet-4',
                  'Outlet-5', 'Outlet-6', 'Outlet-7', 'Outlet-8']
 
+        session.set_status('running')
         self.is_streaming = True
         while self.is_streaming:
             yield dsleep(1)
             read_time = time.time()
 
-            # Check if 60 seconds has passed before getting status
-            if (read_time - self.lastGet) < 60:
+            # Check if sample period has passed before getting status
+            if (read_time - self.lastGet) < self.sample_period:
                 continue
 
             get_list = []
@@ -257,11 +275,15 @@ class ibootbarAgent:
             # Do not publish if ibootbar connection has dropped
             try:
                 # Update session.data
-                oid_cache = update_cache(get_result, names, read_time)
+                oid_cache = update_cache(get_result, names, self.outlet_locked, read_time)
+                oid_cache['address'] = self.address
                 session.data = oid_cache
                 self.log.debug("{data}", data=session.data)
-                self.lastGet = time.time()
 
+                if get_result is None:
+                    raise ConnectionError('No SNMP response. Check your connection.')
+
+                self.lastGet = time.time()
                 # Publish to feed
                 message = _build_message(get_result, names, read_time)
                 self.log.debug("{msg}", msg=message)
@@ -280,8 +302,12 @@ class ibootbarAgent:
         """_stop_acq()
         **Task** - Stop task associated with acq process.
         """
-        self.is_streaming = False
-        return True, "Stopping Recording"
+        if self.is_streaming:
+            session.set_status('stopping')
+            self.is_streaming = False
+            return True, "Stopping Recording"
+        else:
+            return False, "Acq is not currently running"
 
     @ocs_agent.param('outlet', choices=[1, 2, 3, 4, 5, 6, 7, 8])
     @ocs_agent.param('state', choices=['on', 'off'])
@@ -294,13 +320,19 @@ class ibootbarAgent:
         Parameters
         ----------
         outlet : int
-            Index of outlet to set
+            Outlet number to set. Choices are 1-8 (physical outlets).
         state : str
-            State to set outlet to
+            State to set outlet to, which may be 'on' or 'off'
         """
         with self.lock.acquire_timeout(3, job='set_outlet') as acquired:
             if not acquired:
                 return False, "Could not acquire lock"
+
+            # Check if outlet is locked
+            outlet_id = params['outlet'] - 1
+            if self.outlet_locked[outlet_id]:
+                return False, 'Outlet {} is locked. Cannot turn outlet on/off.'.format(params['outlet'])
+
             # Convert given state parameter to integer
             if params['state'] == 'on':
                 state = 1
@@ -308,15 +340,15 @@ class ibootbarAgent:
                 state = 0
 
             # Issue SNMP SET command to given outlet
-            outlet = [('IBOOTPDU-MIB', 'outletControl', params['outlet'] - 1)]
+            outlet = [('IBOOTPDU-MIB', 'outletControl', outlet_id)]
             setcmd = yield self.snmp.set(outlet, self.version, state)
             self.log.info('{}'.format(setcmd))
 
-        # Force SNMP GET status commands by rewinding the lastGet time by 60 seconds
-        self.lastGet = self.lastGet - 60
+        # Force SNMP GET status commands by rewinding the lastGet time by sample period
+        self.lastGet = self.lastGet - self.sample_period
 
         return True, 'Set outlet {} to {}'.\
-            format(params['outlet'] - 1, params['state'])
+            format(params['outlet'], params['state'])
 
     @ocs_agent.param('outlet', choices=[1, 2, 3, 4, 5, 6, 7, 8])
     @ocs_agent.param('cycle_time', default=10, type=int)
@@ -329,32 +361,38 @@ class ibootbarAgent:
         Parameters
         ----------
         outlet : int
-            Index of outlet to cycle
+            Outlet number to cycle. Choices are 1-8 (physical outlets).
         cycle_time : int
             The amount of seconds to cycle an outlet. Default is 10 seconds.
         """
         with self.lock.acquire_timeout(3, job='cycle_outlet') as acquired:
             if not acquired:
                 return False, "Could not acquire lock"
+
+            # Check if outlet is locked
+            outlet_id = params['outlet'] - 1
+            if self.outlet_locked[outlet_id]:
+                return False, 'Outlet {} is locked. Cannot cycle outlet.'.format(params['outlet'])
+
             # Issue SNMP SET command for cycle time
-            set_cycle = [('IBOOTPDU-MIB', 'outletCycleTime', params['outlet'] - 1)]
+            set_cycle = [('IBOOTPDU-MIB', 'outletCycleTime', outlet_id)]
             setcmd1 = yield self.snmp.set(set_cycle, self.version, params['cycle_time'])
             self.log.info('{}'.format(setcmd1))
 
             # Issue SNMP SET command to given outlet
-            outlet = [('IBOOTPDU-MIB', 'outletControl', params['outlet'] - 1)]
+            outlet = [('IBOOTPDU-MIB', 'outletControl', outlet_id)]
             setcmd2 = yield self.snmp.set(outlet, self.version, 2)
             self.log.info('{}'.format(setcmd2))
             self.log.info('Cycling outlet {} for {} seconds'.
-                          format(params['outlet'] - 1, params['cycle_time']))
+                          format(outlet_id, params['cycle_time']))
 
         # Force SNMP GET status commands throughout the cycle time
         for i in range(params['cycle_time'] + 1):
-            self.lastGet = self.lastGet - 60
+            self.lastGet = self.lastGet - self.sample_period
             yield dsleep(1)
 
         return True, 'Cycled outlet {} for {} seconds'.\
-            format(params['outlet'] - 1, params['cycle_time'])
+            format(params['outlet'], params['cycle_time'])
 
     @ocs_agent.param('_')
     @inlineCallbacks
@@ -369,15 +407,43 @@ class ibootbarAgent:
         with self.lock.acquire_timeout(3, job='reboot') as acquired:
             if not acquired:
                 return False, "Could not acquire lock"
+
             # Send SNMP SET command to set rebootSystem to True
             reboot = [('IBOOTPDU-MIB', 'rebootSystem', 0)]
             setcmd = yield self.snmp.set(reboot, self.version, 1)
             self.log.info('{}'.format(setcmd))
 
         # Force SNMP GET status commands
-        self.lastGet = self.lastGet - 60
+        self.lastGet = self.lastGet - self.sample_period
 
         return True, 'Rebooting. Outlets will be set to their initial states.'
+
+    @ocs_agent.param('outlet', choices=[1, 2, 3, 4, 5, 6, 7, 8])
+    @ocs_agent.param('lock', choices=[True, False])
+    def lock_outlet(self, session, params=None):
+        """lock_outlet(outlet, lock)
+
+        **Task** - Lock/unlocks a particular outlet, preventing change of state.
+
+        Parameters
+        ----------
+        outlet : int
+            Outlet number to lock/unlock. Choices are 1-8 (physical outlets).
+        lock : bool
+            Set to true to lock, set to false to unlock
+        """
+        with self.lock.acquire_timeout(3, job='lock_outlet') as acquired:
+            if not acquired:
+                return False, "Could not acquire lock"
+            # Lock/unlock specific outlet
+            outlet = params['outlet'] - 1
+            if params['lock']:
+                self.outlet_locked[outlet] = True
+            elif not params['lock']:
+                self.outlet_locked[outlet] = False
+
+        return True, 'Set outlet {} lock to {}'.\
+            format(params['outlet'], params['lock'])
 
 
 def add_agent_args(parser=None):
@@ -395,7 +461,9 @@ def add_agent_args(parser=None):
     pgroup.add_argument("--snmp-version", default='2', choices=['1', '2', '3'],
                         help="SNMP version for communication. Must match "
                              + "configuration on the ibootbar.")
-    pgroup.add_argument("--mode", choices=['acq', 'test'])
+    pgroup.add_argument("--mode", default='acq', choices=['acq', 'test'])
+    pgroup.add_argument("--lock-outlet", nargs='+', type=int,
+                        help="List of outlets to lock on startup.")
 
     return parser
 
@@ -418,7 +486,8 @@ def main(args=None):
     p = ibootbarAgent(agent,
                       address=args.address,
                       port=int(args.port),
-                      version=int(args.snmp_version))
+                      version=int(args.snmp_version),
+                      lock_outlet=args.lock_outlet)
 
     agent.register_process("acq",
                            p.acq,
@@ -428,6 +497,7 @@ def main(args=None):
     agent.register_task("set_outlet", p.set_outlet, blocking=False)
     agent.register_task("cycle_outlet", p.cycle_outlet, blocking=False)
     agent.register_task("set_initial_state", p.set_initial_state, blocking=False)
+    agent.register_task("lock_outlet", p.lock_outlet, blocking=False)
 
     runner.run(agent, auto_reconnect=True)
 
