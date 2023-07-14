17732fe96cf8b85162ac83fe2d7c6a8a79d8146e
DaveBoettger <42744413+DaveBoettger@users.noreply.github.com>
2023-07-14 14:38:04 -0400

    Changes to allow for reading more than one continuous block of resgis… (#473)
    * Changes to allow for reading more than one continuous block of resgisters at a time
    Support reading of bit or bit range within individual registers needed for reading i.e. alarm conditions from the generator controller
    Configuration file loading switched to read an entire directory of .yaml files at a time
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Replaced lambda function definitions in favor of real function definitions
    in order to satisify flake8 pre-commit.ci tests.
    Manual merge executed.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Better documentation
    
    Added a configuration section to generator.rst
    Added sample configuration files in sample_configs directory.
    Sample interval is now an argument to the agent.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Small changes to docs formatting and args
    
    ---------
    
    Co-authored-by: David Boettger <dave@pop-os.localdomain>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: jlashner <jlashner@gmail.com>

--- socs/agents/generator/agent.py
+++ socs/agents/generator/agent.py
@@ -12,39 +12,108 @@ from pyModbusTCP.client import ModbusClient
 byteorder = sys.byteorder
 
 
+def load_configs(dir_name, config_extension='yaml'):
+    '''Loads all register configuration files form the specified directory (path).
+    The configurations are returned as a list of the configurations from individual files.'''
+
+    config_dir = os.environ.get('OCS_CONFIG_DIR')
+    path = os.path.join(config_dir, 'generator_config', dir_name)
+
+    all_configs = []
+
+    ls = os.listdir(path)
+
+    # Make a filter so that we only try to load files ending in config_extension
+    def filt(f_name):
+        return f_name.endswith(config_extension)
+    # Filter
+    configs = filter(filt, ls)
+    # Load configurations from the remaining files
+    for f in configs:
+        read_config = os.path.join(path, f)
+        with open(read_config) as f:
+            try:
+                data = yaml.load(f, Loader=yaml.SafeLoader)
+            except BaseException:
+                pass
+            all_configs.append(data)
+    return all_configs
+
+
 def twos(val, bytes):
-    """Take an unsigned integer representation of a two's compilment integer
-    and return the correctly signed integer"""
+    '''Take an unsigned integer representation of a two's compilment integer and return the correctly signed integer'''
     b = val.to_bytes(bytes, byteorder=byteorder, signed=False)
     return int.from_bytes(b, byteorder=byteorder, signed=True)
 
 
 def interp_unsigned_double_reg(r1, r2):
-    """Take two 16 bit register values and combine them assuming we really
-    wanted to read a 32 bit unsigned register"""
+    '''Take two 16 bit register values and combine them assuming we really wanted to read a 32 bit unsigned register'''
     return (r1 << 16) + r2
 
 
 def interp_signed_double_reg(r1, r2):
-    """Take two 16 bit register values and combine them assuming we really
-    wanted to read a 32 bit signed register"""
+    '''Take two 16 bit register values and combine them assuming we really wanted to read a 32 bit signed register'''
     return twos(interp_unsigned_double_reg(r1, r2), 4)
 
 
-class ReadString(object):
+def make_bin_reader(offset, spec):
+    '''Read an individual bit or continuous range of bits out of the 2 byte register.
+    A single bit can be specified as a single number between 1 and 16, and a range can
+    be specified with a dash, i.e. X-Y where both X and Y are in the range
+    1 to 16 and X < Y. In either case the bit or range of bits will be returned as an
+    integer.'''
+    spec = spec.split(' ')[1:]
+    spec = spec[0].split('-')
+    spec = [int(s) for s in spec]
+    if len(spec) == 1:
+        # Process individual bit
+        spec = spec[0]
+        # The mask leaves only the desired bit
+        mask = sum([1 << s for s in range(spec - 1, spec)])
 
-    def __init__(self, config, error_out_of_range=True):
-        self.name = config['string_name']
-        self.read_start = config['read_start']
+        def reader(val):
+            return (val[offset] & mask) >> spec - 1
+        return reader
+    elif len(spec) == 2:
+        # Process range
+        low = spec[0]
+        high = spec[1]
+        if low >= high:
+            raise ValueError('First bit in range specification must be smaller than last.')
+        # The mask leaves only the desired bits
+        mask = sum([1 << s for s in range(low - 1, high)])
+
+        def reader(val):
+            return (val[offset] & mask) >> low - 1
+        return reader
+    else:
+        raise ValueError('Cannot read binary read_as specification; use single bit or continuous range.')
+
+
+class ReadBlock(object):
+    '''An object for reading, converting, and evaluating information from a single contiouous block of registers'''
+
+    def __init__(self, config, error_out_of_range=True, filter_errors=True):
+        self.name = config['block_name']
+
+        try:
+            self.read_start = config['read_start']
+        except KeyError:
+            self.read_start = config['page'] * 256
+
         self.read_len = config['read_len']
         self.functions = []
         self.rconfig = config['registers']
-        self.error_val = -1
+        self.error_val = None
         self.error_out_of_range = error_out_of_range
+        self.filter_errors = filter_errors
         for i in self.rconfig:
             self.functions.append(self.build_reader_function(i, self.rconfig[i]))
 
-    def build_reader_function(self, offset, rconfig):
+    def build_reader_function(self, name, rconfig):
+        '''Build and return a closure around a function that converts a specified piece of information from the block'''
+        offset = rconfig['offset']
+
         if rconfig['read_as'] == '16U':
             def evaluator(registers):
                 return registers[offset]
@@ -57,6 +126,8 @@ class ReadString(object):
         elif rconfig['read_as'] == '32S':
             def evaluator(registers):
                 return interp_signed_double_reg(registers[offset], registers[offset + 1])
+        elif 'bin' in rconfig['read_as']:
+            evaluator = make_bin_reader(offset, rconfig['read_as'])
         else:
             def evaluator(registers):
                 return self.error_val
@@ -68,7 +139,7 @@ class ReadString(object):
 
             if self.error_out_of_range:
                 try:
-                    if val < rconfig['min_va']:
+                    if val < rconfig['min_val']:
                         val = self.error_val
                 except KeyError:
                     pass
@@ -79,18 +150,26 @@ class ReadString(object):
                 except KeyError:
                     pass
 
-            return {rconfig['name'].replace(' ', '_'): {'value': val, 'units': rconfig['units']}}
+            if val != self.error_val or not self.filter_errors:
+                return {name: {'value': float(val), 'units': rconfig['units']}}
+            else:
+                return None
 
         return process
 
     def read(self, client):
-        regdata = client.read_holding_registers(self.read_start, self.read_len)
-        regs = regdata
+        # Perform the read for the entire block
+        registers = client.read_holding_registers(self.read_start, self.read_len)
         return_data = {}
         try:
-            for i in range(self.read_len):
-                return_data.update(self.functions[i](regs))
+            # Iterate through the functions that convert and return the individual pieces of
+            # data from this block
+            for f in self.functions:
+                this_data = f(registers)
+                if this_data is not None:
+                    return_data.update(this_data)
         except Exception as e:
+            # print(registers)
             print(f'Error in processing data: {e}')
 
         return return_data
@@ -105,44 +184,62 @@ class Generator:
         Address of the generator controller.
     port : int
         Port to generator controller, default to 5021.
+    config_dir : string
+        Sub-directory of .yaml configuration files that specify blocks of registers to read
+        and specifies how to convert them into useable data.
+    block_space_time : float
+        Amount of time (in seconds) to wait between issuing seperate read_multiple_registers
+        commands to the device. DSE device appears to crash without this waiting period.
+    close_port : boolean
+       Whether or not to close the open port to the device while waiting for the next Pacemaker
+       triggered read cycle. The idea here is that closing the port alllows DSEWebNet to function
+       in parallel with the agent.
 
     Attributes
     ----------
-    read_strings : list
-        Strings of registers defined in config to read from
+    read_blocks : list
+        List of ReadBlock objects that represent the different continuous register locations to
+        read from as specified in the config files.
     client : ModbusClient
         ModbusClient object that initializes connection
     """
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, config_dir, block_space_time=.1, close_port=True):
         self.host = host
         self.port = port
-        self.read_strings = []
-
+        self.read_config = load_configs(config_dir)
         self._build_config()
-
+        self.close_port = close_port
+        self.block_space_time = block_space_time
         self.client = ModbusClient(self.host, self.port, auto_open=True, auto_close=False)
         self.client.open()
 
     def _build_config(self):
-        read_config = os.path.join(os.path.dirname(__file__), "config.yaml")
-        with open(read_config) as f:
-            data = yaml.load(f, Loader=yaml.SafeLoader)
-        for string in [data]:
-            self.read_strings.append(ReadString(string))
+        self.read_blocks = []
+        for block in self.read_config:
+            self.read_blocks.append(ReadBlock(block))
 
     def read_cycle(self):
-        for i, val in enumerate(self.read_strings):
+        this_cycle_data = {}
+        for i, val in enumerate(self.read_blocks):
             data = self._read_regs(val)
-            return data
+            this_cycle_data.update(data)
+            time.sleep(self.block_space_time)  # A gap in time is required between individual requests,
+            # i.e. a pause between reading each read_multiple_registers command.
+        return this_cycle_data
 
-    def _read_regs(self, register_string_obj):
+    def _read_regs(self, register_block_object):
+        if self.close_port:
+            self.client.open()
         try:
-            data = register_string_obj.read(self.client)
+            data = register_block_object.read(self.client)
         except Exception as e:
             print('error in read', e)
+            if self.close_port:
+                self.client.open()
             return
-
+        if self.close_port:
+            self.client.open()
         return data
 
 
@@ -153,10 +250,15 @@ class GeneratorAgent:
     ----------
     agent : OCSAgent
         OCSAgent object which forms this Agent
+    configdir : str
+        Directory where .yaml configuration files specific to this generator instance
+        are stored.
     host : str
         Address of the generator controller.
     port : int
         Port to generator controller, default to 5021.
+    sample_interval : float
+        Time between samples in seconds.
 
     Attributes
     ----------
@@ -169,7 +271,7 @@ class GeneratorAgent:
         txaio logger object, created by the OCSAgent
     """
 
-    def __init__(self, agent, host='localhost', port=5021):
+    def __init__(self, agent, configdir, host='localhost', port=5021, sample_interval=10.):
 
         self.host = host
         self.port = port
@@ -177,6 +279,10 @@ class GeneratorAgent:
         self.agent: ocs_agent.OCSAgent = agent
         self.log = agent.log
         self.lock = TimeoutLock()
+
+        self.configdir = configdir
+
+        self.pacemaker_freq = 1. / sample_interval
 
         self.initialized = False
         self.take_data = False
@@ -196,7 +302,7 @@ class GeneratorAgent:
         Instantiates Generator object and check if client is open
         """
 
-        self.generator = Generator(self.host, self.port)
+        self.generator = Generator(self.host, self.port, config_dir=self.configdir)
         if self.generator.client.is_open:
             self.initialized = True
         else:
@@ -261,11 +367,6 @@ class GeneratorAgent:
              "address": 'localhost',
              "timestamp":1601925677.6914878}
 
-        Refer to the config file at `socs/agents/generator/config.yaml`_
-        for all possible fields and their respective min/max values and units.
-        Note: -1 will be returned for readings out of range.
-
-        .. _socs/agents/generator/config.yaml: https://github.com/simonsobs/socs/blob/main/socs/agents/generator/config.yaml
 
         """
 
@@ -281,7 +382,7 @@ class GeneratorAgent:
 
             session.data = {"fields": {}}
 
-            pm = Pacemaker(1, quantize=True)
+            pm = Pacemaker(self.pacemaker_freq)
             while self.take_data:
                 pm.sleep()
 
@@ -306,11 +407,10 @@ class GeneratorAgent:
                     session.data['address'] = self.host
 
                     regdata = self.generator.read_cycle()
-
                     if regdata:
                         for reg in regdata:
                             data['data'][reg] = regdata[reg]["value"]
-                            field_dict = {reg: regdata[reg]}
+                            field_dict = {reg: regdata[reg]['value']}
                             session.data['fields'].update(field_dict)
                         session.data.update({'timestamp': current_time})
                     else:
@@ -351,6 +451,8 @@ def make_parser(parser=None):
                         help="Port to listen on.")
     pgroup.add_argument('--mode', type=str, choices=['idle', 'init', 'acq'],
                         help="Starting action for the agent.")
+    pgroup.add_argument("--configdir", type=str, help="Path to directory containing .yaml config files.")
+    pgroup.add_argument("--sample-interval", type=float, default=10., help="Time between samples in seconds.")
 
     return parser
 
@@ -376,8 +478,10 @@ def main(args=None):
     agent, runner = ocs_agent.init_site_agent(args)
 
     p = GeneratorAgent(agent,
+                       configdir=args.configdir,
                        host=args.host,
-                       port=int(args.port))
+                       port=int(args.port),
+                       sample_interval=args.sample_interval)
 
     agent.register_task('init_generator', p.init_generator,
                         startup=init_params)
