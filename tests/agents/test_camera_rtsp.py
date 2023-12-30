a2c180fc7ad06fef4e5e5f23ce3adfbeb0d6aee2
Theodore Kisner <tskisner.public@gmail.com>
2023-12-30 09:43:48 -0800

    Address silly flake8 complaints

--- tests/agents/test_camera_rtsp.py
+++ tests/agents/test_camera_rtsp.py
@@ -1,7 +1,6 @@
 try:
-    import cv2
-    import imutils
-
+    import cv2 # noqa: F401
+    import imutils # noqa: F401
     from socs.agents.camera_rtsp.agent import CameraRTSPAgent  # noqa: F401
 except ImportError:
     print("Opencv / imutils not available- skipping CameraRTSPAgent tests")
