74f0d7ebd4f2a50e6518457af9f0e1ffa0510f8a
ykyohei <38639108+ykyohei@users.noreply.github.com>
2024-01-09 10:39:24 -0300

    read twice in relay_read

--- socs/agents/hwp_pcu/drivers/hwp_pcu.py
+++ socs/agents/hwp_pcu/drivers/hwp_pcu.py
@@ -59,6 +59,7 @@ class PCU:
         self.port.write(cmd.encode('utf-8'))
         self.sleep()
         response = self.read()
+        response = self.read()
         if response == "on":
             return 1
         elif response == "off":
