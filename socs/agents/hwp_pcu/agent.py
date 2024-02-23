ccde1baea9747730ec89ccf1e2e5ce40fbce1d54
Jack Lashner <jlashner@gmail.com>
2024-02-23 15:21:01 -0500

    Move callbacks to reactor in PCU agent (#632)

--- socs/agents/hwp_pcu/agent.py
+++ socs/agents/hwp_pcu/agent.py
@@ -75,10 +75,10 @@ class HWPPCUAgent:
             try:
                 self.log.info(f"Running action {action}")
                 res = process_action(action, PCU)
-                action.deferred.callback(res)
+                reactor.callFromThread(action.deferred.callback, res)
             except Exception as e:
                 self.log.error(f"Error processing action: {action}")
-                action.deferred.errback(e)
+                reactor.callFromThread(action.deferred.errback, e)
 
     def _get_and_publish_data(self, PCU: pcu.PCU, session):
         now = time.time()
