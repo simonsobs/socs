ec6a33193c0a83f664b87ba89c0509b72ff96351
Jack Lashner <jlashner@gmail.com>
2024-12-20 15:31:01 -0500

    Lingering subprocess detector

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True, scope='module')
-def check_subprocesses(request, capsys):
+def check_subprocesses():
     # Get a list of child PIDs before the test starts
     parent = psutil.Process(os.getpid())
     before_children = {child.pid for child in parent.children(recursive=True)}
@@ -20,6 +20,4 @@ def check_subprocesses(request, capsys):
         leftover_details = [
             f"PID {p.pid}: {p.cmdline()}" for p in parent.children(recursive=True) if p.pid in leftover_processes
         ]
-        with capsys.disabled():
-            print(f"Lingering subprocesses detected:\n" + "\n".join(leftover_details))
-        pytest.fail()
+        pytest.fail(f"Lingering subprocesses detected:\n" + "\n".join(leftover_details))
