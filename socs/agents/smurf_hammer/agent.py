76e28cd3a6bdb45eac834d5b99d95799f42b6c0c
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2026-08-07 13:08:59 -0700

    fix(agents/smurf_hammer): Don't require access level for now.

--- socs/agents/smurf_hammer/agent.py
+++ socs/agents/smurf_hammer/agent.py
@@ -241,7 +241,8 @@ def main(args=None):
                            startup=startup)
 
     # restrict access to level 2
-    agent.register_task('hammer', p.hammer, min_privs=2)
+    # TODO: not supported yet
+    #agent.register_task('hammer', p.hammer, min_privs=2)
 
     runner.run(agent, auto_reconnect=True)
 
