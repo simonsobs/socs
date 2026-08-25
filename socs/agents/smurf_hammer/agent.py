e57ad68e625f430625f6e5ad4434715fa4ea3d26
Tristan Pinsonneault-Marotte <tristpinsm@gmail.com>
2026-08-25 15:11:38 -0700

    fix(agents/smurf_hammer): Restore register_task.

--- socs/agents/smurf_hammer/agent.py
+++ socs/agents/smurf_hammer/agent.py
@@ -240,9 +240,7 @@ def main(args=None):
                            blocking=False,
                            startup=startup)
 
-    # restrict access to level 2
-    # TODO: not supported yet
-    # agent.register_task('hammer', p.hammer, min_privs=2)
+    agent.register_task('hammer', p.hammer)
 
     runner.run(agent, auto_reconnect=True)
 
